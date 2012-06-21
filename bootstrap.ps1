2b1504091053ea57d5ac055ddd2267f639ff8ddd
unknown <mwrock@wrockdesk.redmond.corp.microsoft.com>
2012-06-21 08:21:27 -0700

    more enhancements to bootstrap

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,26 +2,25 @@ param(
     [string]$sku="dev",
     [switch]$justFinishedUpdates
 )
+Start-Transcript -path $env:temp\transcript.log -Append
 $scriptPath = (Split-Path -parent $MyInvocation.MyCommand.path)
 . $scriptPath\utilities.ps1
 . $scriptPath\VsixInstallFunctions.ps1
+Import-Module $scriptPath\PinnedApplications.psm1
+if(${env:ProgramFiles(x86)} -ne $null){ $programFiles86 = ${env:ProgramFiles(x86)} } else { $programFiles86 = $env:ProgramFiles }
 
 if($justFinishedUpdates -eq $false){
-    Start-Transcript -path $scriptPath/transcript.log
-    Import-Module $scriptPath\PinnedApplications.psm1
-    Disable-UAC
-    Configure-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
-    iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
-    Import-Module $env:systemdrive\chocolatey\chocolateyinstall\helpers\chocolateyInstaller.psm1
-    if(${env:ProgramFiles(x86)} -ne $null){ $programFiles86 = ${env:ProgramFiles(x86)} } else { $programFiles86 = $env:ProgramFiles }
-
     switch ($sku) {
         "Light" { #skuu for wife, kids or grandma
+            Disable-UAC
+            Configure-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
             Choc googlechrome 
             Set-PinnedApplication -Action PinToTaskbar -FilePath "$programFiles86\Google\Chrome\Application\chrome.exe"
         }
         "dev" { #super ultra beta dev sku
-            Enable-Net35-Win7
+            Disable-UAC
+            Configure-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+            Enable-Net35
             Enable-IIS-Win7
             Enable-Telnet-Win7
             Set-PinnedApplication -Action PinToTaskbar -FilePath "$env:windir\system32\mstsc.exe"
@@ -67,14 +66,13 @@ if($justFinishedUpdates -eq $false){
             Install-ChocolateyPackage 'resharper' 'msi' '/quiet' 'http://download.jetbrains.com/resharper/ReSharperSetup.7.0.20.111.msi' 
             InstallVsixSilently http://visualstudiogallery.msdn.microsoft.com/463c5987-f82b-46c8-a97e-b1cde42b9099/file/66837/1/xunit.runner.visualstudio.vsix xunit.runner.visualstudio.vsix, "11.0"
             Choc TestDriven.Net
+
+            Force-Windows-update -GetUpdatesFromMS
         }
+
     }
 }
-else {
-    Start-Transcript -path $scriptPath/transcript.log -Append
-}
 
-Force-Windows-update -GetUpdatesFromMS
 if( Test-Path $scriptPath\bootstrap\post-bootstrap.ps1) {
     Invoke-Expression "$scriptPath\post-bootstrap.ps1 $args"
 }
