55bbc349cf3f262d17d340739c39c34a75d024f6
Matt Wrock <matt@mattwrock.com>
2012-08-05 08:57:32 -0700

    fix typo

--- AdminProxy.ps1
+++ AdminProxy.ps1
@@ -3,7 +3,7 @@ $identity  = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $principal = New-Object System.Security.Principal.WindowsPrincipal( $identity )
 $isAdmin = $principal.IsInRole( [System.Security.Principal.WindowsBuiltInRole]::Administrator )
 $command = "Import-Module `"$scriptPath\BoxStarter.psm1`";Invoke-BoxStarter $args"
-if(-$isAdmin) {  
+if($isAdmin) {  
   Invoke-Expression $command
 }
 else {
