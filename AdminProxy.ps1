31b71c6d1533b970a5255e5a3f61bfed28bc1ea6
Matt Wrock <matt@mattwrock.com>
2012-08-05 08:42:34 -0700

    do not open new admin window if user is already admin

---
+++ AdminProxy.ps1
@@ -0,0 +1,12 @@
+$scriptPath = (Split-Path -parent $MyInvocation.MyCommand.path)
+$identity  = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object System.Security.Principal.WindowsPrincipal( $identity )
+$isAdmin = $principal.IsInRole( [System.Security.Principal.WindowsBuiltInRole]::Administrator )
+$command = "Import-Module `"$scriptPath\BoxStarter.psm1`";Invoke-BoxStarter $args"
+if(-$isAdmin) {  
+  Invoke-Expression $command
+}
+else {
+  $command = "-ExecutionPolicy bypass -noexit -command " + $command
+  Start-Process powershell -verb runas -argumentlist $command
+}
--- BoxStarter.bat
+++ BoxStarter.bat
@@ -1,3 +1,3 @@
 @echo off
   
-powershell -NoProfile -ExecutionPolicy bypass -Command "start-process powershell -verb runas -argumentlist '-noexit -ExecutionPolicy bypass -command ""Import-Module %~dp0BoxStarter.psm1;Invoke-BoxStarter %*""'"
+powershell -NoProfile -ExecutionPolicy bypass -file %~dp0AdminProxy.ps1 %*
