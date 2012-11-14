dbec531fcc7498a3748898c31db17fb26d550fc6
Matt Wrock <matt@mattwrock.com>
2012-11-14 01:03:04 -0800

    added master module

--- BuildScripts/default.ps1
+++ BuildScripts/default.ps1
@@ -14,10 +14,12 @@ Task Package -depends Version-Module, Pack-Nuget, Unversion-Module -description 
 
 Task Version-Module -description 'Stamps the psm1 with the version and last changeset SHA' {
     (Get-Content "$baseDir\Helpers\boxstarter.helpers.psm1") | % {$_ -replace "\`$version\`$", "$version" } | % {$_ -replace "\`$sha\`$", "$changeset" } | Set-Content "$baseDir\helpers\boxstarter.helpers.psm1"
+    (Get-Content "$baseDir\bootstrapper\boxstarter.psm1") | % {$_ -replace "\`$version\`$", "$version" } | % {$_ -replace "\`$sha\`$", "$changeset" } | Set-Content "$baseDir\bootstrapper\boxstarter.psm1"    
 }
 
 Task Unversion-Module -description 'Removes the versioning from the psm1' {
     (Get-Content "$baseDir\helpers\boxstarter.helpers.psm1") | % {$_ -replace "$version", "`$version`$" } | % {$_ -replace "$changeset", "`$sha`$" } | Set-Content "$baseDir\helpers\boxstarter.helpers.psm1"
+    (Get-Content "$baseDir\bootstrapper\boxstarter.psm1") | % {$_ -replace "$version", "`$version`$" } | % {$_ -replace "$changeset", "`$sha`$" } | Set-Content "$baseDir\bootstrapper\boxstarter.psm1"
 }
 
 Task Pack-Nuget -description 'Packs the module and example package' {
@@ -31,6 +33,7 @@ Task Pack-Nuget -description 'Packs the module and example package' {
     exec { .$nugetExe pack "$baseDir\BuildPackages\example-light\example-light.nuspec" -OutputDirectory "$baseDir\BuildPackages" -NoPackageAnalysis -version $version }    
     mkdir "$baseDir\buildArtifacts"
     exec { .$nugetExe pack "$baseDir\helpers\boxstarter.helpers.nuspec" -OutputDirectory "$baseDir\buildArtifacts" -NoPackageAnalysis -version $version }
+    exec { .$nugetExe pack "$baseDir\boxstarter.nuspec" -OutputDirectory "$baseDir\buildArtifacts" -NoPackageAnalysis -version $version }
 }
 
 Task Push-Nuget -description 'Pushes the module to Myget feed' {
