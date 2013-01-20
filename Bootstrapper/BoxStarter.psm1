9e9f4e1aea2a2e5b65711cd0236cad5c82281523
Matt Wrock <matt@mattwrock.com>
2013-01-20 12:54:12 -0800

    added command line help to Invoke-Boxstarter

--- Bootstrapper/BoxStarter.psm1
+++ Bootstrapper/BoxStarter.psm1
@@ -9,6 +9,31 @@ $baseDir = (Split-Path -parent $PSScriptRoot)
 $config = $configXml.config
 
 function Invoke-BoxStarter{
+<#
+.SYNOPSIS
+Invokes the installation of a Boxstarter package
+
+.DESCRIPTION
+This essentially wraps Chocolatey Install and provides these additional features
+ - Installs chocolatey if it is not already installed
+ - Installs the .net 4.0 framework if it is not installed which is a chocolatey requirement
+ - Turns off the windows update service during installation to prevent installation conflicts and minimize the need for reboots
+ - Imports the Boxstarter.Helpers module that provides functions for customizing windows
+ - Provides Reboot Resiliency by ensuring the package installation is immediately restarted up on reboot if there is a reboot during the installation.
+
+ The .nupkg file for the provided package name is searched in the following locations and order:
+ - .\BuildPackages relative to the parent directory of the module file
+ - The chocolatey feed
+ - The boxstarter feed on myget
+
+ .PARAMETER bootstrapPackage
+ The package to be installed.
+ The .nupkg file for the provided package name is searched in the following locations and order:
+ - .\BuildPackages relative to the parent directory of the module file
+ - The chocolatey feed
+ - The boxstarter feed on myget
+
+#>    
     param(
       [string]$bootstrapPackage="default"
     )
