48bf788347fa8e9a92a3161145eb75aad29017c7
Matt Wrock <matt@mattwrock.com>
2013-02-02 00:12:40 -0800

    should use test drive

--- tests/Invoke-BoxStarter.tests.ps1
+++ tests/Invoke-BoxStarter.tests.ps1
@@ -1,16 +1,18 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 if(get-module Boxstarter){Remove-Module boxstarter}
 ."$here\..\build.bat" Pack-Nuget
-Copy-Item $here\..\BuildArtifacts\Boxstarter.Helpers.*.nupkg $here
-Copy-Item $here\..\BuildArtifacts\test-package.*.nupkg $here
 
 Describe "Invoke-Boxstarter" {
+    $testRoot = (Get-PSDrive TestDrive).Root
+    mkdir "$testRoot\Repo"
+    Copy-Item $here\..\BuildArtifacts\Boxstarter.Helpers.*.nupkg "$testRoot\Repo"
+    Copy-Item $here\..\BuildArtifacts\test-package.*.nupkg "$testRoot\Repo"
     Context "When installing a local package" {
         remove-Item "$env:ChocolateyInstall\lib\test-package.*" -force -recurse
         remove-Item "$env:ChocolateyInstall\lib\boxstarter.helpers.*" -force -recurse
         Add-Content "$env:ChocolateyInstall\ChocolateyInstall\ChocolateyInstall.log" -Value "______ test-package v1.0.0 ______" -force
 
-        ."$here\..\boxstarter.bat" test-package $here
+        ."$here\..\boxstarter.bat" test-package "$testRoot\Repo"
 
         it "should save boxstarter package to chocolatey lib folder" {
             (Test-Path "$env:ChocolateyInstall\lib\test-package.*").Should.Be($true)
@@ -23,5 +25,4 @@ Describe "Invoke-Boxstarter" {
             $installLines.Should.Have_Count_Of(1)
         }          
     }
-    remove-Item "$here\*.nupkg" -force
 }
