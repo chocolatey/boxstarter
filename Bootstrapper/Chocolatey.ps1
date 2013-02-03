77e849bc521ca86a3cab7c57fa9e2eafaacaebb6
Matt Wrock <matt@mattwrock.com>
2013-02-03 13:00:24 -0800

    add synopsis for chocolatey interceptors

--- Bootstrapper/Chocolatey.ps1
+++ Bootstrapper/Chocolatey.ps1
@@ -1,16 +1,36 @@
 function cinst {
+<#
+.SYNOPSIS
+Intercepts Chocolatey call to check for reboots
+
+#>    
     chocolatey Install @args
 }
 
 function cup {
+<#
+.SYNOPSIS
+Intercepts Chocolatey call to check for reboots
+
+#>    
     chocolatey Update @args
 }
 
 function cinstm {
+<#
+.SYNOPSIS
+Intercepts Chocolatey call to check for reboots
+
+#>    
     chocolatey InstallMissing @args
 }
 
 function chocolatey {
+<#
+.SYNOPSIS
+Intercepts Chocolatey call to check for reboots
+
+#>    
     if(Test-PendingReboot) {return Invoke-Reboot}
     Call-Chocolatey @args
 }
