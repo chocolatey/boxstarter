570c7cf31ad48731a0c99b0d35c96052bd7a097a
Matt Wrock <matt@mattwrock.com>
2013-02-04 03:40:22 -0800

    fix ad hoc test for multi reboot scenario

--- BuildPackages/ad-hoc-tests/tools/chocolateyInstall.ps1
+++ BuildPackages/ad-hoc-tests/tools/chocolateyInstall.ps1
@@ -1,9 +1,10 @@
 if(!(Test-Path "c:\users\mwrock\test1.txt")) {
-    new-Item c:\users\mwrock\test1.txt -value "hi1"
+    new-Item c:\users\mwrock\test1.txt -value "hi1" -type file
     return Invoke-Reboot
 }
 if(!(Test-Path "c:\users\mwrock\test2.txt")) {
-    new-Item c:\users\mwrock\test2.txt -value "hi1"
+    new-Item c:\users\mwrock\test2.txt -value "hi1" -type file
     return Invoke-Reboot
 }
 write-host "I am done"
+del c:\users\mwrock\test*
