ab434ee7a31d08e8ae15ebedb4392b4dcf62c39f
Matt Wrock <matt@mattwrock.com>
2013-02-09 12:02:20 -0800

    fix vs ctp update to send silent params and to check if already installed

--- BuildPackages/example/tools/chocolateyInstall.ps1
+++ BuildPackages/example/tools/chocolateyInstall.ps1
@@ -6,8 +6,10 @@ Set-TaskbarSmall
 Enable-RemoteDesktop
 
 cinstm VisualStudio2012Ultimate
-if(Test-PendingReboot){Invoke-Reboot}
-Install-ChocolateyPackage 'vs update 2 ctp2' 'exe' 'http://download.microsoft.com/download/8/9/3/89372D24-6707-4587-A7F0-10A29EECA317/vsupdate_KB2707250.exe'
+if((Get-Item "$($Boxstarter.programFiles86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe").VersionInfo.ProductVersion -lt "11.0.60115.1") {
+    if(Test-PendingReboot){Invoke-Reboot}
+    Install-ChocolateyPackage 'vs update 2 ctp2' 'exe' '/passive /norestart' 'http://download.microsoft.com/download/8/9/3/89372D24-6707-4587-A7F0-10A29EECA317/vsupdate_KB2707250.exe'
+}
 cinstm fiddler
 cinstm mssqlserver2012express
 cinstm office2013ProPlusPreview
