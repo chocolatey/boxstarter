82d171fd2006e179201c27a0036d77a34ba466da
Matt Wrock <matt@mattwrock.com>
2013-02-09 17:49:50 -0800

    suppress meaningless output from enabling rdp

--- Helpers/Enable-RemoteDesktop.ps1
+++ Helpers/Enable-RemoteDesktop.ps1
@@ -7,6 +7,6 @@ Allows Remote Desktop access to machine and enables Remote Desktop firewall rule
 http://boxstarter.codeplex.com
 
 #>
-    (Get-WmiObject -Class "Win32_TerminalServiceSetting" -Namespace root\cimv2\terminalservices).SetAllowTsConnections(1)
-    netsh advfirewall firewall set rule group="Remote Desktop" new enable=yes
+    (Get-WmiObject -Class "Win32_TerminalServiceSetting" -Namespace root\cimv2\terminalservices).SetAllowTsConnections(1) | out-null
+    netsh advfirewall firewall set rule group="Remote Desktop" new enable=yes | out-null
 }
