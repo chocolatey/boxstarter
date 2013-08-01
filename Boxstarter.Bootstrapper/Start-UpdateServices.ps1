2200416bb30e0158681ee95a9f9318f5fd8d8797
Matt Wrock <matt@mattwrock.com>
2013-08-01 01:16:15 -0700

    only try to restart services if they are not running

--- Boxstarter.Bootstrapper/Start-UpdateServices.ps1
+++ Boxstarter.Bootstrapper/Start-UpdateServices.ps1
@@ -1,12 +1,15 @@
 function Start-UpdateServices {
-    Write-BoxstarterMessage "Starting Windows Update Service"
-    Enter-BoxstarterLogable { Start-Service -Name wuauserv }
+    $wuaStatus = (Get-Service wuauserv).Status
+    if($wuaStatus -ne "Running"){
+        Write-BoxstarterMessage "Starting Windows Update Service"
+        Enter-BoxstarterLogable { Start-Service -Name wuauserv }
+    }
     Start-CCMEXEC
 }
 
 function Start-CCMEXEC {
     $ccm = get-service -include CCMEXEC
-    if($ccm) {
+    if($ccm -and $ccm.Status -ne "Running") {
         Write-BoxstarterMessage "Starting Configuration Manager Service"
         set-service CCMEXEC -startuptype automatic
         Enter-BoxstarterLogable { Start-Service CCMEXEC }
