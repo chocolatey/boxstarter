c23b59e861486a93f9a6d0024d0e8bb07519df30
Matt Wrock <matt@mattwrock.com>
2013-08-01 01:20:43 -0700

    do not make network call to choco if packages are on disk

--- BuildScripts/build.ps1
+++ BuildScripts/build.ps1
@@ -14,9 +14,9 @@ if(-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall")){
     iex ((new-object net.webclient).DownloadString("http://bit.ly/psChocInstall"))
 }
 
-cinstm psake
-cinstm 7zip.CommandLine
-cinstm pester
+if(!(Test-Path $env:ChocolateyInstall\lib\Psake*)) { cinst psake }
+if(!(Test-Path $env:ChocolateyInstall\lib\7zip.CommandLine*)) { cinst 7zip.CommandLine }
+if(!(Test-Path $env:ChocolateyInstall\lib\pester*)) { cinst pester }
 
 if($Help){ 
   try {
