ae1c783d99b0792cb81a647435985e7d62efc35c
Matt Wrock <matt@mattwrock.com>
2013-10-10 02:51:31 -0700

    finished tests for invoking scheduled task. need to tidy it up a bit

--- tests/Common/Invoke-FromTask.tests.ps1
+++ tests/Common/Invoke-FromTask.tests.ps1
@@ -15,15 +15,33 @@ Describe "Invoke-FromTask" {
         It "Should invoke the command"{
             Get-Content $env:temp\test.txt | should be "this is a test"
         }
+        It "Should delete the task"{
+            schtasks /query /TN 'Ad-Hoc Task' 2>&1 | out-null
+            $LastExitCode | should be 1
+        }
     }
 
-    Context "When Invoking Task with output"{
+    Context "When Invoking a task with output"{
         Remove-Item $env:temp\test.txt -ErrorAction SilentlyContinue
 
         $result=Invoke-FromTask "Write-Output 'here is some output'" -Credential $mycreds -Timeout 0
 
         It "Should invoke the command"{
             $result | should be "here is some output"
+        }
+    }
+
+    Context "When Invoking a task with an error"{
+        Remove-Item $env:temp\test.txt -ErrorAction SilentlyContinue
+
+        try{Invoke-FromTask "Throw 'This is an error'" -Credential $mycreds -Timeout 0 2>&1 | Out-Null} catch { $err=$_}
+
+        It "Should throw the error"{
+            $err.Exception | should match "This is an error"
+        }
+        It "Should delete the task"{
+            schtasks /query /TN 'Ad-Hoc Task' 2>&1 | out-null
+            $LastExitCode | should be 1
         }
     }
 
@@ -49,11 +67,19 @@ Describe "Invoke-FromTask" {
 
     Context "When Invoking Task that is idle longer than timeout"{
         try { Invoke-FromTask "Start-Process calc.exe -Wait" -Credential $mycreds -Timeout 2} catch {$err=$_}
+        $origId=Get-WmiObject -Class Win32_Process -Filter "name = 'powershell.exe' and CommandLine like '%-EncodedCommand%'" | select ProcessId | % { $_.ProcessId }
         $id=Get-WmiObject -Class Win32_Process -Filter "Name='calc.exe'" | select ProcessId | % { $_.ProcessId }
         KILL $id
 
         It "Should timeout"{
             $err.Exception | should match "likely in a hung state"
+        }
+        It "Should delete the task"{
+            schtasks /query /TN 'Ad-Hoc Task' 2>&1 | out-null
+            $LastExitCode | should be 1
+        }
+        It "Should kill the original powershell task"{
+            $origId | should be $null
         }
     }
 }
