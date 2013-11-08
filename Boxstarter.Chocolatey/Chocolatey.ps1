75d58883e7972a4ce4aa6f8da79285626f78c2ac
Matt Wrock <matt@mattwrock.com>
2013-11-08 00:20:21 -0800

    silently continue on error when deleting failed package

--- Boxstarter.Chocolatey/Chocolatey.ps1
+++ Boxstarter.Chocolatey/Chocolatey.ps1
@@ -176,7 +176,7 @@ function Add-DefaultRebootCodes($codes) {
 function Remove-ChocolateyPackageInProgress($packageName) {
     $pkgDir = (dir $env:ChocolateyInstall\lib\$packageName.*)
     if($pkgDir.length -gt 0) {$pkgDir = $pkgDir[-1]}
-    remove-item $pkgDir -Recurse -Force    
+    remove-item $pkgDir -Recurse -Force -ErrorAction SilentlyContinue  
 }
 
 function Expand-Splat($splat){
