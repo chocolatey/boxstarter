6ed58c09859d0c002d330822eb4843b18c64caa8
Matt Wrock <matt@mattwrock.com>
2013-12-02 01:23:07 -0800

    remove the verbose preference when enabling remoting on target and wait for the job to finish before continuing

--- Boxstarter.Chocolatey/Enable-RemotePsRemoting.ps1
+++ Boxstarter.Chocolatey/Enable-RemotePsRemoting.ps1
@@ -30,56 +30,58 @@ param(
     [Management.Automation.PsCredential]$Credential
 )
 
-$VerbosePreference = "Continue"
+    $credential = Get-Credential $credential
+    $username = $credential.Username
+    $password = $credential.GetNetworkCredential().Password
 
-$credential = Get-Credential $credential
-$username = $credential.Username
-$password = $credential.GetNetworkCredential().Password
+    $script = @"
 
-$script = @"
+    `$log = Join-Path `$env:TEMP Enable-RemotePsRemoting.output.txt
+    Remove-Item -Force `$log -ErrorAction SilentlyContinue
+    Start-Transcript -Path `$log
 
-`$log = Join-Path `$env:TEMP Enable-RemotePsRemoting.output.txt
-Remove-Item -Force `$log -ErrorAction SilentlyContinue
-Start-Transcript -Path `$log
+    ## Create a task that will run with full network privileges.
+    ## In this task, we call Enable-PsRemoting
+    schtasks /CREATE /TN 'Enable Remoting' /SC WEEKLY /RL HIGHEST ``
+        /RU $username /RP $password ``
+        /TR "powershell -noprofile -command Enable-PsRemoting -Force" /F |
+        Out-String
+    schtasks /RUN /TN 'Enable Remoting' | Out-String
 
-## Create a task that will run with full network privileges.
-## In this task, we call Enable-PsRemoting
-schtasks /CREATE /TN 'Enable Remoting' /SC WEEKLY /RL HIGHEST ``
-    /RU $username /RP $password ``
-    /TR "powershell -noprofile -command Enable-PsRemoting -Force" /F |
-    Out-String
-schtasks /RUN /TN 'Enable Remoting' | Out-String
+    `$securePass = ConvertTo-SecureString $password -AsPlainText -Force
+    `$credential =
+        New-Object Management.Automation.PsCredential $username,`$securepass
 
-`$securePass = ConvertTo-SecureString $password -AsPlainText -Force
-`$credential =
-    New-Object Management.Automation.PsCredential $username,`$securepass
+    ## Wait for the remoting changes to come into effect
+    for(`$count = 1; `$count -le 10; `$count++)
+    {
+        `$output = Invoke-Command localhost { 1 } -Cred `$credential ``
+            -ErrorAction SilentlyContinue
+        if(`$output -eq 1) { break; }
 
-## Wait for the remoting changes to come into effect
-for(`$count = 1; `$count -le 10; `$count++)
-{
-    `$output = Invoke-Command localhost { 1 } -Cred `$credential ``
-        -ErrorAction SilentlyContinue
-    if(`$output -eq 1) { break; }
+        "Attempt `$count : Not ready yet."
+        Sleep 5
+    }
 
-    "Attempt `$count : Not ready yet."
-    Sleep 5
-}
-
-## Delete the temporary task
-schtasks /DELETE /TN 'Enable Remoting' /F | Out-String
-Stop-Transcript
+    ## Delete the temporary task
+    schtasks /DELETE /TN 'Enable Remoting' /F | Out-String
+    Stop-Transcript
 
 "@
 
-$commandBytes = [System.Text.Encoding]::Unicode.GetBytes($script)
-$encoded = [Convert]::ToBase64String($commandBytes)
+    $commandBytes = [System.Text.Encoding]::Unicode.GetBytes($script)
+    $encoded = [Convert]::ToBase64String($commandBytes)
 
-Write-Verbose "Configuring $computername"
-$command = "powershell -NoProfile -EncodedCommand $encoded"
-$null = Invoke-WmiMethod -Computer $computername -Credential $credential `
-    Win32_Process Create -Args $command
-
-Write-Verbose "Testing connection"
-Invoke-Command $computername {
-    Get-WmiObject Win32_ComputerSystem } -Credential $credential
+    Write-Verbose "Configuring $computername"
+    $command = "powershell -NoProfile -EncodedCommand $encoded"
+    $null = Invoke-WmiMethod -Computer $computername -Credential $credential `
+        Win32_Process Create -Args $command
+    Sleep 10
+    Write-Verbose "Testing connection"
+    for($count = 1; $count -le 10; $count++) {
+        $wmiResult = Invoke-Command $computername {
+            Get-WmiObject Win32_ComputerSystem } -Credential $credential -ErrorAction SilentlyContinue
+        if($wmiResult -ne $Null){break}
+        $global:Error.RemoveAt(0)
+    }
 }
