19f851921574ddb2d31805add7d97afb97a1aa46
Matt Wrock <matt@mattwrock.com>
2013-12-02 01:25:13 -0800

    adding initial hyper-v remote enablement implementation

---
+++ Boxstarter.Virtualization/Boxstarter.Virtualization.psd1
@@ -0,0 +1,52 @@
+@{
+Description = 'Provides Cmdlets that wil install a boxstarter package on a Hyper-V VM'
+# Script module or binary module file associated with this manifest.
+ModuleToProcess = './boxstarter.Virtualization.psm1'
+
+# Version number of this module.
+ModuleVersion = '1.1.35'
+
+# ID used to uniquely identify this module
+GUID = 'bbdb3e8b-9daf-4c00-a553-4f3f88fb6e58'
+
+# Author of this module
+Author = 'Matt Wrock'
+
+# Copyright statement for this module
+Copyright = '(c) 2013 Matt Wrock'
+
+# Minimum version of the Windows PowerShell engine required by this module
+PowerShellVersion = '3.0'
+
+# Minimum version of the .NET Framework required by this module
+DotNetFrameworkVersion = '4.0'
+
+# Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
+NestedModules = @('..\Boxstarter.Common\Boxstarter.Common.psd1','..\Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1')
+
+# Functions to export from this module
+FunctionsToExport = '*'
+
+# Cmdlets to export from this module
+CmdletsToExport = '*'
+
+# Variables to export from this module
+VariablesToExport = '*'
+
+# Aliases to export from this module
+AliasesToExport = '*'
+
+# List of all modules packaged with this module.
+# ModuleList = @()
+
+# List of all files packaged with this module
+# FileList = @()
+
+# Private data to pass to the module specified in RootModule/ModuleToProcess
+PrivateData = 'ef8692790f5ea4cb022f86953b5ce20a501b7ca4'
+
+# Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
+# DefaultCommandPrefix = ''
+
+}
+
---
+++ Boxstarter.Virtualization/Install-BoxstarterVM.ps1
@@ -0,0 +1,75 @@
+function Enable-BoxstarterVM {
+    [CmdletBinding()]
+    param(
+        [string]$vmName,
+        [string]$vmCheckpoint
+    )
+    $CurrentVerbosity=$global:VerbosePreference
+
+    try {
+        if($PSBoundParameters['Verbose']) {
+            $global:VerbosePreference="Continue"
+        }
+
+        if($vm -eq $null){
+            throw New-Object -TypeName InvalidOperationException -ArgumentList "Could not fine VM: $vmName"
+        }
+        $vm=Get-VM $vmName -ErrorAction SilentlyContinue
+        if($vm.State -eq "saved"){
+            Remove-VMSavedState $vmName
+        }
+        else {
+            Stop-VM $VmName -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+        }
+        $vhd=Get-VMHardDiskDrive -VMName $vmName
+        $VHDPath = $vhd.Path
+
+        if((Get-ItemProperty $VHDPath -Name IsReadOnly).IsReadOnly){
+            throw New-Object -TypeName InvalidOperationException -ArgumentList "The VHD is Read-Only"
+        }    
+        $before = (Get-Volume).DriveLetter
+        mount-vhd $VHDPath
+        $after = (Get-Volume).DriveLetter
+        $winVolume = compare $before $after -Passthru
+        try{
+            Get-PSDrive | Out-Null
+            $winVolume = $winVolume | ? {Test-Path "$($_):\windows\System32\config"}
+            if($winVolume -eq $null){
+                throw New-Object -TypeName InvalidOperationException -ArgumentList "The VHD does not contain system volume"
+            }    
+            Write-BoxstarterMessage "Mounted VHD with system volume to Drive $($winVolume)" -Verbose
+            reg load HKLM\VHDSOFTWARE "$($winVolume):\windows\system32\config\software" | out-null
+            $policyResult = reg add HKLM\VHDSOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
+            Write-BoxstarterMessage "Enabled LocalAccountTokenFilterPolicy with result: $policyResult" -Verbose
+
+            reg load HKLM\VHDSYS "$($winVolume):\windows\system32\config\system" | out-null
+            $current=(Get-ItemProperty "HKLM:\VHDSYS\Select" -Name Current).Current
+            $computerName = (Get-ItemProperty "HKLM:\VHDSYS\ControlSet00$current\Control\ComputerName\ComputerName" -Name ComputerName).ComputerName
+
+            $rules = Get-ItemProperty "HKLM:\VHDSYS\ControlSet00$current\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules"
+            $rule=$rules.'WMI-RPCSS-In-TCP'
+            $newVal = $rule.Replace("|Active=FALSE|","|Active=TRUE|")
+            Set-ItemProperty "HKLM:\VHDSYS\ControlSet00$current\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" -Name WMI-RPCSS-In-TCP -Value $newVal
+            Write-BoxstarterMessage "Changed WMI-RPCSS-In-TCP firewall rule to: $newVal" -Verbose
+
+            $rule=$rules.'WMI-WINMGMT-In-TCP'
+            $newVal = $rule.Replace("|Active=FALSE|","|Active=TRUE|")
+            Set-ItemProperty "HKLM:\VHDSYS\ControlSet00$current\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" -Name WMI-WINMGMT-In-TCP -Value $newVal
+            Write-BoxstarterMessage "Changed WMI-WINMGMT-In-TCP firewall rule to: $newVal" -Verbose
+        }
+        finally{
+            [GC]::Collect() # The next line will fail without this since handles to the loaded hive have not yet been collected
+            reg unload HKLM\VHDSOFTWARE 2>&1 | out-null
+            reg unload HKLM\VHDSYS 2>&1 | out-null
+            Write-BoxstarterMessage "VHD Registry Unloaded"
+            Dismount-VHD $VHDPath
+            Write-BoxstarterMessage "VHD Dismounted"
+        }
+        Start-VM $VmName
+        $result = Wait-Port $computerName 135 45000
+        return "$computerName"
+    }
+    finally{
+        $global:VerbosePreference=$CurrentVerbosity
+    }
+}
