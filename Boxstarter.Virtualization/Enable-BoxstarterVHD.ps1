e8511193cbbb4f25d8b68a393b9ae35f45826c6f
Matt Wrock <matt@mattwrock.com>
2013-12-02 23:55:40 -0800

    extract VHD editing to separate function

---
+++ Boxstarter.Virtualization/Enable-BoxstarterVHD.ps1
@@ -0,0 +1,60 @@
+function Enable-BoxstarterVHD {
+    [CmdletBinding()]
+    param([string]$VHDPath)
+    Invoke-Verbosely -Verbose:($PSBoundParameters['Verbose'] -eq $true) {
+        if((Get-ItemProperty $VHDPath -Name IsReadOnly).IsReadOnly){
+            throw New-Object -TypeName InvalidOperationException -ArgumentList "The VHD is Read-Only"
+        }    
+        $before = (Get-Volume).DriveLetter
+        mount-vhd $VHDPath
+        $after = (Get-Volume).DriveLetter
+        $winVolume = compare $before $after -Passthru
+        try{
+            Get-PSDrive | Out-Null
+            $winVolume = $winVolume | ? {Test-Path "$($_):\windows\System32\config"}
+            if($winVolume -eq $null){
+                throw New-Object -TypeName InvalidOperationException -ArgumentList "The VHD does not contain system volume"
+            }    
+            Write-BoxstarterMessage "Mounted VHD with system volume to Drive $($winVolume)" -Verbose
+            reg load HKLM\VHDSOFTWARE "$($winVolume):\windows\system32\config\software" | out-null
+            $policyResult = reg add HKLM\VHDSOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
+            Write-BoxstarterMessage "Enabled LocalAccountTokenFilterPolicy with result: $policyResult" -Verbose
+
+            reg load HKLM\VHDSYS "$($winVolume):\windows\system32\config\system" | out-null
+            $current=Get-CurrentControlSet
+            $computerName = (Get-ItemProperty "HKLM:\VHDSYS\ControlSet00$current\Control\ComputerName\ComputerName" -Name ComputerName).ComputerName
+
+            Enable-FireWallRule WMI-RPCSS-In-TCP
+            Enable-FireWallRule WMI-WINMGMT-In-TCP
+
+            return "$computerName"
+        }
+        finally{
+            [GC]::Collect() # The next line will fail without this since handles to the loaded hive have not yet been collected
+            reg unload HKLM\VHDSOFTWARE 2>&1 | out-null
+            reg unload HKLM\VHDSYS 2>&1 | out-null
+            Write-BoxstarterMessage "VHD Registry Unloaded" -Verbose
+            Dismount-VHD $VHDPath
+            Write-BoxstarterMessage "VHD Dismounted" -Verbose
+        }
+    }
+}
+
+function Enable-FireWallRule($ruleName){
+    $key=Get-FirewallKey
+    $rules = Get-ItemProperty $key
+    $rule=$rules.$ruleName
+    $newVal = $rule.Replace("|Active=FALSE|","|Active=TRUE|")
+    Set-ItemProperty $key -Name $ruleName -Value $newVal
+    Write-BoxstarterMessage "Changed $ruleName firewall rule to: $newVal" -Verbose
+}
+
+function Get-FireWallKey{
+    $current = Get-CurrentControlSet
+    return "HKLM:\VHDSYS\ControlSet00$current\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules"
+}
+
+function Get-CurrentControlSet {
+    return (Get-ItemProperty "HKLM:\VHDSYS\Select" -Name Current).Current
+
+}
--- Boxstarter.Virtualization/Enable-BoxstarterVM.ps1
+++ Boxstarter.Virtualization/Enable-BoxstarterVM.ps1
@@ -20,49 +20,8 @@ function Enable-BoxstarterVM {
             Stop-VM $VmName -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
         }
         $vhd=Get-VMHardDiskDrive -VMName $vmName
-        $VHDPath = $vhd.Path
 
-        if((Get-ItemProperty $VHDPath -Name IsReadOnly).IsReadOnly){
-            throw New-Object -TypeName InvalidOperationException -ArgumentList "The VHD is Read-Only"
-        }    
-        $before = (Get-Volume).DriveLetter
-        mount-vhd $VHDPath
-        $after = (Get-Volume).DriveLetter
-        $winVolume = compare $before $after -Passthru
-        try{
-            Get-PSDrive | Out-Null
-            $winVolume = $winVolume | ? {Test-Path "$($_):\windows\System32\config"}
-            if($winVolume -eq $null){
-                throw New-Object -TypeName InvalidOperationException -ArgumentList "The VHD does not contain system volume"
-            }    
-            Write-BoxstarterMessage "Mounted VHD with system volume to Drive $($winVolume)" -Verbose
-            reg load HKLM\VHDSOFTWARE "$($winVolume):\windows\system32\config\software" | out-null
-            $policyResult = reg add HKLM\VHDSOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
-            Write-BoxstarterMessage "Enabled LocalAccountTokenFilterPolicy with result: $policyResult" -Verbose
-
-            reg load HKLM\VHDSYS "$($winVolume):\windows\system32\config\system" | out-null
-            $current=(Get-ItemProperty "HKLM:\VHDSYS\Select" -Name Current).Current
-            $computerName = (Get-ItemProperty "HKLM:\VHDSYS\ControlSet00$current\Control\ComputerName\ComputerName" -Name ComputerName).ComputerName
-
-            $rules = Get-ItemProperty "HKLM:\VHDSYS\ControlSet00$current\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules"
-            $rule=$rules.'WMI-RPCSS-In-TCP'
-            $newVal = $rule.Replace("|Active=FALSE|","|Active=TRUE|")
-            Set-ItemProperty "HKLM:\VHDSYS\ControlSet00$current\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" -Name WMI-RPCSS-In-TCP -Value $newVal
-            Write-BoxstarterMessage "Changed WMI-RPCSS-In-TCP firewall rule to: $newVal" -Verbose
-
-            $rule=$rules.'WMI-WINMGMT-In-TCP'
-            $newVal = $rule.Replace("|Active=FALSE|","|Active=TRUE|")
-            Set-ItemProperty "HKLM:\VHDSYS\ControlSet00$current\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" -Name WMI-WINMGMT-In-TCP -Value $newVal
-            Write-BoxstarterMessage "Changed WMI-WINMGMT-In-TCP firewall rule to: $newVal" -Verbose
-        }
-        finally{
-            [GC]::Collect() # The next line will fail without this since handles to the loaded hive have not yet been collected
-            reg unload HKLM\VHDSOFTWARE 2>&1 | out-null
-            reg unload HKLM\VHDSYS 2>&1 | out-null
-            Write-BoxstarterMessage "VHD Registry Unloaded" -Verbose
-            Dismount-VHD $VHDPath
-            Write-BoxstarterMessage "VHD Dismounted" -Verbose
-        }
+        $computerName = Enable-BoxstarterVHD $vhd.Path
         Start-VM $VmName
         Write-BoxstarterMessage "Started $VMName. Waiting for Heartbeat..."
         do {Start-Sleep -milliseconds 100} 
