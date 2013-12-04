10fe91cdaa33ef26c4538fb4a510d14034075c0e
Matt Wrock <matt@mattwrock.com>
2013-12-04 00:25:50 -0800

    if checkpoint does not exist, assume a new checkpoint should be created

--- Boxstarter.Virtualization/Enable-BoxstarterVM.ps1
+++ Boxstarter.Virtualization/Enable-BoxstarterVM.ps1
@@ -7,10 +7,14 @@ function Enable-BoxstarterVM {
     Invoke-Verbosely -Verbose:($PSBoundParameters['Verbose'] -eq $true) {
         $vm=Get-VM $vmName -ErrorAction SilentlyContinue
         if($vm -eq $null){
-            throw New-Object -TypeName InvalidOperationException -ArgumentList "Could not fine VM: $vmName"
+            throw New-Object -TypeName InvalidOperationException -ArgumentList "Could not find VM: $vmName"
         }
-        if($vmCheckpoint -ne $null){
-            Restore-VMSnapshot $vm -Name $vmCheckpoint -Confirm:$false
+        if($vmCheckpoint -ne $null -and $vmCheckpoint.Length -gt 0){
+            $point = Get-VMSnapshot -VMName $vmName -Name $vmCheckpoint -ErrorAction SilentlyContinue
+            if($point -ne $null) {
+                Restore-VMSnapshot $vm -Name $vmCheckpoint -Confirm:$false
+                $restored=$true
+            }
         }
         if($vm.State -eq "saved"){
             Remove-VMSavedState $vmName
@@ -26,6 +30,10 @@ function Enable-BoxstarterVM {
         Write-BoxstarterMessage "Started $VMName. Waiting for Heartbeat..."
         do {Start-Sleep -milliseconds 100} 
         until ((Get-VMIntegrationService $vm | ?{$_.name -eq "Heartbeat"}).PrimaryStatusDescription -eq "OK")
+        if(!$restored) {
+            Write-BoxstarterMessage "Creating Checkpoint $vmCheckpoint"
+            Checkpoint-VM -Name $vmName -SnapshotName $vmCheckpoint
+        }
         return "$computerName"
     }
 }
