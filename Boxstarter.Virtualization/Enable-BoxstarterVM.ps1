7f32be8a619e700465fb7d5b5d8d356e16c5fc9b
Matt Wrock <matt@mattwrock.com>
2013-12-04 01:23:58 -0800

    document the enable-BoxstarterVM command

--- Boxstarter.Virtualization/Enable-BoxstarterVM.ps1
+++ Boxstarter.Virtualization/Enable-BoxstarterVM.ps1
@@ -1,8 +1,72 @@
 function Enable-BoxstarterVM {
+<#
+.SYNOPSIS
+Opens WMI ports and LocalAccountTokenFilterPolicy for workgroup Hyper-V VMs
+
+.DESCRIPTION
+Prepares a Hyper-V VM for Boxstarter Installation. Opening WMI 
+ports if remoting is not enabled and enabling 
+LocalAccountTokenFilterPolicy if the VM is not in a domain so 
+that Boxstarter can later enable PowerShell Remoting.
+
+Enable-BoxstarterVM will also restore the VM to a specified 
+checkpoint or create a new checkpoint if the given checkpoint 
+does not exist.
+
+.PARAMETER VMName
+The name of the VM to enable.
+
+.PARAMETER Credential
+The Credential to use to test PSRemoting.
+
+.PARAMETER CheckpointName
+If a Checkpoint exists by this name, it will be restored. Otherwise one will be created.
+
+.NOTES
+PSRemoting mut be enabled in order for Boxstarter to install to a remote machine. Bare 
+Metal machines require a manual step of enabling it before remote Boxstarter installs 
+will work. However, on a Hyper-V VM, Boxstarter can manage this by mounting and 
+manipulating the VM's VHD. Boxstarter can open the WMI ports which enable it to create a 
+Scheduled Task that will enable PSRemoting. For VMs that are not domain joined, 
+Boxstarter will also enable LocalAccountTokenFilterPolicy so that local accounts can 
+authenticate remotely.
+
+For Non-HyperV VMs, use Enable-BoxstarterVHD to perform these adjustments on the VHD of 
+the VM. The VM must be powered off and accesible.
+
+.OUTPUTS
+A BoxstarterConnectionTokens object that contains the DNS Name of the VM Computer and 
+the PSCredential needed to authenticate.
+
+.EXAMPLE
+Enable-BoxstarterVM MyVM $cred
+
+Prepares MyVM for a Boxstarter Installation
+
+.EXAMPLE
+Enable-BoxstarterVM MyVM $cred | Install-BoxstarterPackage MyPackage
+
+Prepares MyVM and then installs MyPackage
+
+.EXAMPLE
+Enable-BoxstarterVM MyVM $cred ExistingSnapshot | Install-BoxstarterPackage MyPackage
+
+Prepares MyVM, Restores ExistingSnapshot and then installs MyPackage
+
+.EXAMPLE
+Enable-BoxstarterVM MyVM $cred NewSnapshot | Install-BoxstarterPackage MyPackage
+
+Prepares MyVM, Creates a new snapshot named NewSnapshot and then installs MyPackage
+
+.LINK
+http://boxstarter.codeplex.com
+
+#>
     [CmdletBinding()]
     param(
         [string]$VMName,
-        [string]$VMCheckpoint
+        [Management.Automation.PsCredential]$Credential,
+        [string]$CheckpointName
     )
     Invoke-Verbosely -Verbose:($PSBoundParameters['Verbose'] -eq $true) {
         $vm=Get-VM $vmName -ErrorAction SilentlyContinue
@@ -15,10 +79,10 @@ function Enable-BoxstarterVM {
         #Test remoting
         #Test WMI
 
-        if($vmCheckpoint -ne $null -and $vmCheckpoint.Length -gt 0){
-            $point = Get-VMSnapshot -VMName $vmName -Name $vmCheckpoint -ErrorAction SilentlyContinue
+        if($CheckpointName -ne $null -and $CheckpointName.Length -gt 0){
+            $point = Get-VMSnapshot -VMName $vmName -Name $CheckpointName -ErrorAction SilentlyContinue
             if($point -ne $null) {
-                Restore-VMSnapshot $vm -Name $vmCheckpoint -Confirm:$false
+                Restore-VMSnapshot $vm -Name $CheckpointName -Confirm:$false
                 $restored=$true
             }
         }
@@ -38,7 +102,7 @@ function Enable-BoxstarterVM {
         until ((Get-VMIntegrationService $vm | ?{$_.name -eq "Heartbeat"}).PrimaryStatusDescription -eq "OK")
         if(!$restored) {
             Write-BoxstarterMessage "Creating Checkpoint $vmCheckpoint"
-            Checkpoint-VM -Name $vmName -SnapshotName $vmCheckpoint
+            Checkpoint-VM -Name $vmName -SnapshotName $CheckpointName
         }
         return "$computerName"
     }
