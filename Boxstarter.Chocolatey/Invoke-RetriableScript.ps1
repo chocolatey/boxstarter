41d861e3d35f65d1a7ac205e0a0fe6859eaca3ba
Matt Wrock <matt@mattwrock.com>
2013-12-20 13:55:50 -0800

    add retriable script function to vs proj and add a comment

--- Boxstarter.Chocolatey/Invoke-RetriableScript.ps1
+++ Boxstarter.Chocolatey/Invoke-RetriableScript.ps1
@@ -1,4 +1,10 @@
-function Invoke-RetriableScript([ScriptBlock]$RetryScript){
+function Invoke-RetriableScript{
+<#
+.SYNOPSIS
+Retries a script 5 times or until it completes without terminating errors. 
+All Unnamed ars will be passed as arguments to the script
+#>
+    param([ScriptBlock]$RetryScript)
     for($count = 1; $count -le 5; $count++) {
         try {
             Write-BoxstarterMessage "Attempt #$count..." -Verbose
