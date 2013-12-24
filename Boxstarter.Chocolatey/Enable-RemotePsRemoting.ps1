71a03054bac23b82c17b26112e2169ec9ca3643b
Matt Wrock <matt@mattwrock.com>
2013-12-23 22:55:50 -0800

    change network location to Private if it is public when not on a domain and enabling remoting

--- Boxstarter.Chocolatey/Enable-RemotePsRemoting.ps1
+++ Boxstarter.Chocolatey/Enable-RemotePsRemoting.ps1
@@ -40,6 +40,13 @@ param(
     Remove-Item -Force `$log -ErrorAction SilentlyContinue
     Start-Transcript -Path `$log
 
+    if(!(1,3,4,5 -contains (Get-WmiObject win32_computersystem).DomainRole)) { 
+        `$networkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]'{DCB00C01-570F-4A9B-8D69-199FDBA5723B}')) 
+        `$connections = `$networkListManager.GetNetworkConnections() 
+
+        `$connections | % {`$_.GetNetwork().SetCategory(1)}
+    } 
+
     ## Create a task that will run with full network privileges.
     ## In this task, we call Enable-PsRemoting
     schtasks /CREATE /TN 'Enable Remoting' /SC WEEKLY /RL HIGHEST ``
