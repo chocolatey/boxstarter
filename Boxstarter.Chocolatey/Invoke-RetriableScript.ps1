8acdb02552adcfc563aa86ac8c257917cb4a0f73
Matt Wrock <matt@mattwrock.com>
2013-12-24 23:11:48 -0800

    make sure to catch non terminating errors in retriable scripts

--- Boxstarter.Chocolatey/Invoke-RetriableScript.ps1
+++ Boxstarter.Chocolatey/Invoke-RetriableScript.ps1
@@ -5,17 +5,24 @@ Retries a script 5 times or until it completes without terminating errors.
 All Unnamed ars will be passed as arguments to the script
 #>
     param([ScriptBlock]$RetryScript)
-    for($count = 1; $count -le 5; $count++) {
-        try {
-            Write-BoxstarterMessage "Attempt #$count..." -Verbose
-            $ret = Invoke-Command -ScriptBlock $RetryScript -ArgumentList $args
-            return $ret
-            break
+    $currentErrorAction=$ErrorActionPreference
+    try{
+        $ErrorActionPreference = "Stop"
+        for($count = 1; $count -le 5; $count++) {
+            try {
+                Write-BoxstarterMessage "Attempt #$count..." -Verbose
+                $ret = Invoke-Command -ScriptBlock $RetryScript -ArgumentList $args
+                return $ret
+                break
+            }
+            catch {
+                if($global:Error.Count -gt 0){$global:Error.RemoveAt(0)}
+                if($count -eq 5) { throw $_ }
+                else { Sleep 10 }
+            }
         }
-        catch {
-            if($global:Error.Count -gt 0){$global:Error.RemoveAt(0)}
-            if($count -eq 5) { throw $_ }
-            else { Sleep 10 }
-        }
+    }
+    finally{
+        $ErrorActionPreference = $currentErrorAction
     }
 }
