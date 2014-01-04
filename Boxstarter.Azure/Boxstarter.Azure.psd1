0b4ab56b7ce77ad1c818bd3ea9947c0c2fdf608f
Matt Wrock <matt@mattwrock.com>
2014-01-04 11:17:22 -0800

    adding azure project

---
+++ Boxstarter.Azure/Boxstarter.Azure.psd1
@@ -0,0 +1,52 @@
+@{
+Description = 'Provides Cmdlets that wil install a boxstarter package on a Windows Azure VM'
+# Script module or binary module file associated with this manifest.
+ModuleToProcess = './boxstarter.Azure.psm1'
+
+# Version number of this module.
+ModuleVersion = '2.2.59'
+
+# ID used to uniquely identify this module
+GUID = 'bbdb3e8b-9daf-4c00-a553-4f3f88fb6e59'
+
+# Author of this module
+Author = 'Matt Wrock'
+
+# Copyright statement for this module
+Copyright = '(c) 2013 Matt Wrock'
+
+# Minimum version of the Windows PowerShell engine required by this module
+PowerShellVersion = '3.0'
+
+# Minimum version of the .NET Framework required by this module
+DotNetFrameworkVersion = '4.0'
+
+# Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
+NestedModules = @('..\Boxstarter.Common\Boxstarter.Common.psd1','..\Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1')
+
+# Functions to export from this module
+FunctionsToExport = '*'
+
+# Cmdlets to export from this module
+CmdletsToExport = '*'
+
+# Variables to export from this module
+VariablesToExport = '*'
+
+# Aliases to export from this module
+AliasesToExport = '*'
+
+# List of all modules packaged with this module.
+# ModuleList = @()
+
+# List of all files packaged with this module
+# FileList = @()
+
+# Private data to pass to the module specified in RootModule/ModuleToProcess
+PrivateData = 'd9d8ae1ae8506f094b99797eb19068d468ece1d7'
+
+# Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
+# DefaultCommandPrefix = ''
+
+}
+
---
+++ Boxstarter.Azure/Enable-BoxstarterVM.ps1
@@ -0,0 +1,206 @@
+function Enable-BoxstarterVM {
+<#
+.SYNOPSIS
+Opens WMI ports and LocalAccountTokenFilterPolicy for workgroup Hyper-V VMs
+
+.DESCRIPTION
+Prepares a Hyper-V VM for Boxstarter Installation. Opening WMI 
+ports if remoting is not enabled and enabling 
+LocalAccountTokenFilterPolicy if the VM is not in a domain so 
+that Boxstarter can later enable PowerShell Remoting.
+
+Enable-BoxstarterVM will also restore the VM to a specified 
+checkpoint or create a new checkpoint if the given checkpoint 
+does not exist.
+
+.PARAMETER VMName
+The name of the VM to enable.
+
+.PARAMETER Credential
+The Credential to use to test PSRemoting.
+
+.PARAMETER CheckpointName
+If a Checkpoint exists by this name, it will be restored. Otherwise one will be created.
+
+.NOTES
+PSRemoting mut be enabled in order for Boxstarter to install to a remote machine. Bare 
+Metal machines require a manual step of enabling it before remote Boxstarter installs 
+will work. However, on a Hyper-V VM, Boxstarter can manage this by mounting and 
+manipulating the VM's VHD. Boxstarter can open the WMI ports which enable it to create a 
+Scheduled Task that will enable PSRemoting. For VMs that are not domain joined, 
+Boxstarter will also enable LocalAccountTokenFilterPolicy so that local accounts can 
+authenticate remotely.
+
+For Non-HyperV VMs, use Enable-BoxstarterVHD to perform these adjustments on the VHD of 
+the VM. The VM must be powered off and accesible.
+
+.OUTPUTS
+A BoxstarterConnectionConfig that contains the DNS Name of the VM Computer and 
+the PSCredential needed to authenticate.
+
+.EXAMPLE
+Enable-BoxstarterVM MyVM $cred
+
+Prepares MyVM for a Boxstarter Installation
+
+.EXAMPLE
+Enable-BoxstarterVM MyVM $cred | Install-BoxstarterPackage MyPackage
+
+Prepares MyVM and then installs MyPackage
+
+.EXAMPLE
+Enable-BoxstarterVM MyVM $cred ExistingSnapshot | Install-BoxstarterPackage MyPackage
+
+Prepares MyVM, Restores ExistingSnapshot and then installs MyPackage
+
+.EXAMPLE
+Enable-BoxstarterVM MyVM $cred NewSnapshot | Install-BoxstarterPackage MyPackage
+
+Prepares MyVM, Creates a new snapshot named NewSnapshot and then installs MyPackage
+
+.LINK
+http://boxstarter.codeplex.com
+
+#>
+    [CmdletBinding()]
+    [OutputType([BoxstarterConnectionConfig])]
+    param(
+        [parameter(Mandatory=$true, ValueFromPipeline=$True, Position=0)]
+        [string[]]$VMName,
+        [parameter(Mandatory=$true, Position=1)]
+        [Management.Automation.PsCredential]$Credential,
+        [string]$CheckpointName
+    )
+    Begin {
+        ##Cannot run remotely unelevated. Look into self elevating
+        if(!(Test-Admin)) {
+            Write-Error "You must be running as an administrator. Please open a Powershell console as Administrator and rerun Install-BoxstarperPackage."
+            return
+        }
+
+        if(!(Get-Command -Name Get-VM -ErrorAction SilentlyContinue)){
+            Write-Error "Boxstarter could not find the Hyper-V Powershell Module installed. This is required for use with Boxstarter.HyperV. Run Install-windowsfeature -name hyper-v -IncludeManagementTools."
+            return
+        }
+
+        $CurrentVerbosity=$global:VerbosePreference
+
+        if($PSBoundParameters["Verbose"] -eq $true) {
+            $global:VerbosePreference="Continue"
+        }
+    }
+
+    Process {
+        $VMName | % { 
+
+            $vm=Get-VM $_ -ErrorAction SilentlyContinue
+            if($vm -eq $null){
+                throw New-Object -TypeName InvalidOperationException -ArgumentList "Could not find VM: $_"
+            }
+
+            if($CheckpointName -ne $null -and $CheckpointName.Length -gt 0){
+                $point = Get-VMSnapshot -VMName $_ -Name $CheckpointName -ErrorAction SilentlyContinue
+                $origState=$vm.State
+                if($point -ne $null) {
+                    Restore-VMSnapshot -VMName $_ -Name $CheckpointName -Confirm:$false
+                    Write-BoxstarterMessage "$checkpointName restored on $_ waiting to complete..."
+                    $restored=$true
+                }
+            }
+
+            if($vm.State -eq "saved"){
+                Remove-VMSavedState $_
+            }
+
+            if($vm.State -ne "running"){
+                Start-VM $_ -ErrorAction SilentlyContinue
+                Wait-HeartBeat $_
+            }
+
+            do {
+                Start-Sleep -milliseconds 100
+                $ComputerName=Get-VMGuestComputerName $_
+            } 
+            until ($ComputerName -ne $null)
+            $clientRemoting = Enable-BoxstarterClientRemoting $ComputerName
+            Write-BoxstarterMessage "Testing remoting access on $ComputerName..."
+            $remotingTest = Invoke-Command $ComputerName { Get-WmiObject Win32_ComputerSystem } -Credential $Credential -ErrorAction SilentlyContinue
+        
+            $params=@{}
+            if(!$remotingTest) {
+                Log-BoxstarterMessage "PowerShell remoting connection failed:"
+                if($global:Error.Count -gt 0) { Log-BoxstarterMessage $global:Error[0] }
+                write-BoxstarterMessage "Testing WSMAN..."
+                $WSManResponse = Test-WSMan $ComputerName -ErrorAction SilentlyContinue
+                if($WSManResponse) { 
+                    Write-BoxstarterMessage "WSMAN responded. Will not enable WMI." -verbose
+                    $params["IgnoreWMI"]=$true
+                }
+                else {
+                    Log-BoxstarterMessage "WSMan connection failed:"
+                    if($global:Error.Count -gt 0) { Log-BoxstarterMessage $global:Error[0] }
+                    write-BoxstarterMessage "Testing WMI..."
+                    $wmiTest=try { Invoke-WmiMethod -ComputerName $ComputerName -Credential $Credential Win32_Process Create -Args "cmd.exe" -ErrorAction SilentlyContinue } catch {$ex=$_}
+                    if($wmiTest -or ($ex -ne $null -and $ex.CategoryInfo.Reason -eq "UnauthorizedAccessException")) { 
+                        Write-BoxstarterMessage "WMI responded. Will not enable WMI." -verbose
+                        $params["IgnoreWMI"]=$true
+                    }
+                    else {
+                        Log-BoxstarterMessage "WMI connection failed:"
+                        if($global:Error.Count -gt 0) { Log-BoxstarterMessage $global:Error[0] }
+                    }
+                }
+                $credParts = $Credential.UserName.Split("\\")
+                if(($credParts.Count -eq 1 -and $credParts[0] -eq "administrator") -or `
+                  ($credParts.Count -eq 2 -and $credParts[0] -eq $ComputerName -and $credParts[1] -eq "administrator") -or`
+                  ($credParts.Count -eq 2 -and $credParts[0] -ne $ComputerName)){
+                    $params["IgnoreLocalAccountTokenFilterPolicy"]=$true
+                }
+                if($credParts.Count -eq 2 -and $credParts[0] -eq $ComputerName -and $credParts[1] -eq "administrator"){
+                    $params["IgnoreLocalAccountTokenFilterPolicy"]=$true
+                }
+
+            }
+
+            if(!$remotingTest -and ($params.Count -lt 2)) { 
+                Write-BoxstarterMessage "Stopping $_"
+                Stop-VM $_ -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+                $vhd=Get-VMHardDiskDrive -VMName $_
+                Enable-BoxstarterVHD $vhd.Path @params | Out-Null
+                Start-VM $_
+                Write-BoxstarterMessage "Started $_. Waiting for Heartbeat..."
+                Wait-HeartBeat $_
+            }
+
+            if(!$restored -and $CheckpointName -ne $null -and $CheckpointName.Length -gt 0) {
+                Write-BoxstarterMessage "Creating Checkpoint $vmCheckpoint"
+                Checkpoint-VM -Name $_ -SnapshotName $CheckpointName
+            }
+
+            $res=new-Object -TypeName BoxstarterConnectionConfig -ArgumentList $computerName,$Credential
+            return $res
+        }
+    }
+
+    End {
+        $global:VerbosePreference=$CurrentVerbosity
+    }
+}
+
+function Get-VMGuestComputerName($vmName) {
+    $vm = Get-WmiObject -Namespace root\virtualization\v2 -Class Msvm_ComputerSystem -Filter "ElementName='$vmName'"
+    $vm.GetRelated("Msvm_KvpExchangeComponent").GuestIntrinsicExchangeItems | % {
+        if(([XML]$_) -ne $null){
+            $GuestExchangeItemXml = ([XML]$_).SelectSingleNode("/INSTANCE/PROPERTY[@NAME='Name']/VALUE[child::text()='FullyQualifiedDomainName']") 
+        
+            if ($GuestExchangeItemXml -ne $null) { 
+                $GuestExchangeItemXml.SelectSingleNode("/INSTANCE/PROPERTY[@NAME='Data']/VALUE/child::text()").Value 
+            }
+        }
+    }    
+}
+
+function Wait-HeartBeat($vmName) {
+    do {Start-Sleep -milliseconds 100} 
+    until ((Get-VMIntegrationService -VMName $vmName | ?{$_.name -eq "Heartbeat"}).PrimaryStatusDescription -eq "OK")
+}
