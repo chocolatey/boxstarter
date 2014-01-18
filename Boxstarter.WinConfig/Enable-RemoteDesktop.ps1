e563005b8acaadc1def72d7059eb5cfe7355c928
Matt Wrock <matt@mattwrock.com>
2014-01-18 15:23:19 -0800

    fail gracefully if OS does not have namespace for terminal services and use the second param of SetAllowTsConnections to avoid having to touch the firewall directly

--- Boxstarter.WinConfig/Enable-RemoteDesktop.ps1
+++ Boxstarter.WinConfig/Enable-RemoteDesktop.ps1
@@ -7,6 +7,10 @@ Allows Remote Desktop access to machine and enables Remote Desktop firewall rule
 http://boxstarter.codeplex.com
 
 #>
-    (Get-WmiObject -Class "Win32_TerminalServiceSetting" -Namespace root\cimv2\terminalservices).SetAllowTsConnections(1) | out-null
-    netsh advfirewall firewall set rule group="Remote Desktop" new enable=yes | out-null
+    $obj = Get-WmiObject -Class "Win32_TerminalServiceSetting" -Namespace root\cimv2\terminalservices
+    if($obj -eq $null) {
+        Write-Host "Unable to locate terminalservices namespace. Remote Desktop is not enabled"
+        return
+    }
+    $obj.SetAllowTsConnections(1,1) | out-null
 }
