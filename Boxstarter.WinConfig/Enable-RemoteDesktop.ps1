c60136d13f0d3b82c471cd2d026fc4e01920ea1d
Matt Wrock <matt@mattwrock.com>
2014-01-18 15:33:42 -0800

    use write-boxstarterMessage instead of write-host

--- Boxstarter.WinConfig/Enable-RemoteDesktop.ps1
+++ Boxstarter.WinConfig/Enable-RemoteDesktop.ps1
@@ -9,7 +9,7 @@ http://boxstarter.codeplex.com
 #>
     $obj = Get-WmiObject -Class "Win32_TerminalServiceSetting" -Namespace root\cimv2\terminalservices
     if($obj -eq $null) {
-        Write-Host "Unable to locate terminalservices namespace. Remote Desktop is not enabled"
+        Write-BoxstarterMessage "Unable to locate terminalservices namespace. Remote Desktop is not enabled"
         return
     }
     $obj.SetAllowTsConnections(1,1) | out-null
