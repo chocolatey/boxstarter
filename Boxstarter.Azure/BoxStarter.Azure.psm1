91c745e9000ccd29e5f3156679f453d7263124ff
Matt Wrock <matt@mattwrock.com>
2014-01-19 21:11:43 -0800

    make loading azure module more robust

--- Boxstarter.Azure/BoxStarter.Azure.psm1
+++ Boxstarter.Azure/BoxStarter.Azure.psm1
@@ -2,5 +2,14 @@ $unNormalized=(Get-Item "$PSScriptRoot\..\Boxstarter.Chocolatey\Boxstarter.Choco
 Import-Module $unNormalized.FullName -global -DisableNameChecking -Force
 Resolve-Path $PSScriptRoot\*-*.ps1 | 
     % { . $_.ProviderPath }
-
+if((Get-Module Azure -ListAvailable) -eq $null) {
+    if(${env:ProgramFiles(x86)} -ne $null){ $programFiles86 = ${env:ProgramFiles(x86)} } else { $programFiles86 = $env:ProgramFiles }
+    $modulePath="$programFiles86\Microsoft SDKs\Windows Azure\PowerShell\Azure\Azure.psd1"
+    if(Test-Path $modulePath) {
+        Import-Module $modulePath
+    }
+}
+else {
+    Import-Module Azure
+}
 Export-ModuleMember Enable-BoxstarterVM, Get-AzureVMCheckpoint, Set-AzureVMCheckpoint, Restore-AzureVMCheckpoint, Remove-AzureVMCheckpoint
