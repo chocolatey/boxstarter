eeed07a24c56e0caed7a956bd58e10f26238ec3f
Matt Wrock <matt@mattwrock.com>
2014-01-19 21:44:16 -0800

    change azure nuget to point to propper azure sdk

--- Boxstarter.Azure/Boxstarter.Azure.psd1
+++ Boxstarter.Azure/Boxstarter.Azure.psd1
@@ -4,7 +4,7 @@ Description = 'Provides Cmdlets that wil install a boxstarter package on a Windo
 ModuleToProcess = './boxstarter.Azure.psm1'
 
 # Version number of this module.
-ModuleVersion = '2.2.111'
+ModuleVersion = '2.2.118'
 
 # ID used to uniquely identify this module
 GUID = 'bbdb3e8b-9daf-4c00-a553-4f3f88fb6e59'
@@ -45,7 +45,7 @@ AliasesToExport = '*'
 # FileList = @()
 
 # Private data to pass to the module specified in RootModule/ModuleToProcess
-PrivateData = '8dfa0500ab16bbe6b872fc89c18eb062704f5a8c'
+PrivateData = '91c745e9000ccd29e5f3156679f453d7263124ff'
 
 # Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
 # DefaultCommandPrefix = ''
--- Web/Properties/AssemblyInfo.cs
+++ Web/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Web")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("bb516b69b19fe965feea1cf8d7d581fcf88b6b5f")]
+[assembly: AssemblyTrademark("91c745e9000ccd29e5f3156679f453d7263124ff")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.2.110")]
-[assembly: AssemblyFileVersion("2.2.110")]
+[assembly: AssemblyVersion("2.2.118")]
+[assembly: AssemblyFileVersion("2.2.118")]
