2d582e8f30e05b837e77f5b067278911b9cb92d2
Matt Wrock <matt@mattwrock.com>
2014-01-24 08:22:18 -0800

    import windows azure powershell globally so its commands are exposed outside of boxstarter

--- Boxstarter.Azure/BoxStarter.Azure.psm1
+++ Boxstarter.Azure/BoxStarter.Azure.psm1
@@ -6,10 +6,10 @@ if((Get-Module Azure -ListAvailable) -eq $null) {
     if(${env:ProgramFiles(x86)} -ne $null){ $programFiles86 = ${env:ProgramFiles(x86)} } else { $programFiles86 = $env:ProgramFiles }
     $modulePath="$programFiles86\Microsoft SDKs\Windows Azure\PowerShell\Azure\Azure.psd1"
     if(Test-Path $modulePath) {
-        Import-Module $modulePath
+        Import-Module $modulePath -global
     }
 }
 else {
-    Import-Module Azure
+    Import-Module Azure -global
 }
 Export-ModuleMember Enable-BoxstarterVM, Get-AzureVMCheckpoint, Set-AzureVMCheckpoint, Restore-AzureVMCheckpoint, Remove-AzureVMCheckpoint
