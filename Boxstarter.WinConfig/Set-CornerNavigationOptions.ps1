9ff39ad2b86e7e39743bc4c2bafbc96623e782c7
Gary Ewan Park <gep13@gep13.co.uk>
2014-02-05 20:45:40 +0000

    (#29) Add Corner Navigation to WinConfig Functions
    - Added new Set-CornerNavigationOptions function
    - This takes one of 6 parameters (2 x 3 operations enable/disable) to enable/disable three operations contained within the Navigation tab of the "Taskbar and Navigation Properties" window
    - Updated the WinConfig Module to include new function
    - Updated the WinConfig Web Page to include new function

---
+++ Boxstarter.WinConfig/Set-CornerNavigationOptions.ps1
@@ -0,0 +1,57 @@
+function Set-CornerNavigationOptions {
+<#
+.SYNOPSIS
+Sets options for the Windows Corner Navigation
+
+.PARAMETER EnableUpperRightCornerShowCharms
+When I point to the upper-right corner, show the charms
+
+.PARAMETER DisableUpperRightCornerShowCharms
+Disables the showing of charms when pointing to the upper right corner, see EnableUpperRightCornerShowCharms
+
+.PARAMETER EnableUpperLeftCornerSwitchApps
+When I click the upper-left corner, switch between my recent apps
+
+.PARAMETER DisableUpperLeftCornerSwitchApps
+Disables the switching between recent apps, when clicking in the upper-left corner, see EnableUpperLeftCornerSwitchApps
+
+.PARAMETER EnableLowerLeftCornerWithPowerShell
+Replace Command Prompt with Windows PowerShell in the menu when I right-click the lower-left corner or press Windows key+X
+
+.PARAMETER DisableLowerLeftCornerWithPowerShell
+Disables the showing of Windows PowerShell in the lower-left corner, see EnableLowerLeftCornerWithPowerShell
+#>
+	[CmdletBinding()]
+	param(
+		[switch]$EnableUpperRightCornerShowCharms,
+		[switch]$DisableUpperRightCornerShowCharms,
+		[switch]$EnableUpperLeftCornerSwitchApps,
+		[switch]$DisableUpperLeftCornerSwitchApps,
+		[switch]$EnableLowerLeftCornerWithPowerShell,
+		[switch]$DisableLowerLeftCornerWithPowerShell
+	)
+
+	$PSBoundParameters.Keys | % {
+        if($_-like "En*"){ $other="Dis" + $_.Substring(2)}
+        if($_-like "Dis*"){ $other="En" + $_.Substring(3)}
+        if($PSBoundParameters[$_] -and $PSBoundParameters[$other]) {
+            throw new-Object -TypeName ArgumentException "You may not set both $_ and $other. You can only set one."
+        }
+    }
+
+	$edgeUIKey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\ImmersiveShell\EdgeUi'
+	$advancedKey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+
+	if(Test-Path -Path $edgeUIKey) {
+		if($EnableUpperRightCornerShowCharms) { Set-ItemProperty -Path $edgeUIKey -Name 'DisableTRCorner' -Value 0 }
+		if($DisableUpperRightCornerShowCharms) { Set-ItemProperty -Path $edgeUIKey -Name 'DisableTRCorner' -Value 1 }
+
+		if($EnableUpperLeftCornerSwitchApps) { Set-ItemProperty -Path $edgeUIKey -Name 'DisableTLCorner' -Value 0 }
+		if($DisableUpperLeftCornerSwitchApps) { Set-ItemProperty -Path $edgeUIKey -Name 'DisableTLCorner' -Value 1 }
+	}
+
+	if(Test-Path -Path $advancedKey) {
+		if($EnableLowerLeftCornerWithPowerShell) { Set-ItemProperty -Path $advancedKey -Name 'DontUsePowerShellOnWinX' -Value 0 }
+		if($DisableLowerLeftCornerWithPowerShell) { Set-ItemProperty -Path $advancedKey -Name 'DontUsePowerShellOnWinX' -Value 1 }
+    }
+}
