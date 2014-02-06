bcdf843ce3b47b99c5539615ed91797b97c022f8
Gary Ewan Park <gep13@gep13.co.uk>
2014-02-06 20:04:12 +0000

    (#51 and #52) Refactored Set-ExplorerOptions to Set-WindowsExplorerOptions
    - Added new Set-WindowsExplorerOptions function
    - This takes one of 8 parameters (2 x 4 operations enable/disable) to enable/disable four operations contained within Options of Windows Explorer
    - Updated the WinConfig Module to include new function
    - Updated the WinConfig Web Page to include new function
    - Added re-direct from Set-ExplorerOptions to Set-WindowsExplorerOptions

--- Boxstarter.WinConfig/Set-ExplorerOptions.ps1
+++ Boxstarter.WinConfig/Set-ExplorerOptions.ps1
@@ -21,9 +21,11 @@ http://boxstarter.codeplex.com
         [switch]$showProtectedOSFiles, 
         [switch]$showFileExtensions
     )
-    $key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
-    if($showHidenFilesFoldersDrives) {Set-ItemProperty $key Hidden 1}
-    if($showFileExtensions) {Set-ItemProperty $key HideFileExt 0}
-    if($showProtectedOSFiles) {Set-ItemProperty $key ShowSuperHidden 1}
-    Restart-Explorer
+
+	Write-Warning "This command is deprecated, use Set-WindowsExplorerOptions instead."
+	Write-Warning "Your call to this function will now be routed to the Set-WindowsExplorerOptions function."
+
+	if($showHidenFilesFoldersDrives) { Set-WindowsExplorerOptions -EnableShowHidenFilesFoldersDrives }
+    if($showFileExtensions) { Set-WindowsExplorerOptions -EnableShowProtectedOSFiles }
+    if($showProtectedOSFiles) { Set-WindowsExplorerOptions -EnableShowFileExtensions }
 }
