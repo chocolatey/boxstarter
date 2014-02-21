b127fac2e08523f125bb76dbfbb5ef71c9336925
Matt Wrock <matt@mattwrock.com>
2014-02-21 00:56:28 -0800

    write a message about where the scripts are installed

--- Boxstarter.CI/Install-BoxstarterScripts.ps1
+++ Boxstarter.CI/Install-BoxstarterScripts.ps1
@@ -5,7 +5,10 @@ function Install-BoxstarterScripts {
         [ValidateScript({Test-Path $_})]
         [string]$RepoRootPath
     )
+    
     $scriptPath = Join-Path $RepoRootPath BoxstarterScripts
+    Write-BoxstarterMessage "Copying Boxstarter CI scripts to $scriptPath"
+
     if(!(Test-Path $scriptPath)) { Mkdir $scriptPath | Out-Null }
     Copy-Item "$($Boxstarter.BaseDir)\Boxstarter.CI\bootstrap.ps1" $scriptPath -Force 
     Copy-Item "$($Boxstarter.BaseDir)\Boxstarter.CI\BoxstarterBuild.ps1" $scriptPath -Force 
