a1fe8f1474f0d379fd957d3397c7e0be34555d7a
Matt Wrock <matt@mattwrock.com>
2014-02-24 01:02:08 -0800

    adding messaging to test evaluation results

--- Boxstarter.CI/Test-BoxstarterPackage.ps1
+++ Boxstarter.CI/Test-BoxstarterPackage.ps1
@@ -20,11 +20,17 @@ function Test-BoxstarterPackage {
     $global:testName = ""
 
     if(!$packageName){
+        Write-BoxstarterMessage "Searching for packages that need to be built and tested..."
         $packageName = Get-BoxstarterPackages | ? {
             Test-PackageVersionGreaterThanPublished $_
         } | % {$_.Name}
     }
     
+    if($PackageName.Count -eq 0) {
+        Write-BoxstarterMessage "All packages are up to date. Nothing to build and test."
+        return
+    }
+
     $PackageName | % {
         Invoke-BoxstarterBuild $_
         $p=$_
@@ -51,17 +57,25 @@ function Test-BoxstarterPackage {
 }
 
 function Test-PackageVersionGreaterThanPublished ($package) {
-    if(!$package.Feed) { return $false }
+    if(!$package.Feed) { 
+        Write-BoxstarterMessage "No feed has been assigned to $($package.Name). It will not be build and tested"
+        return $false 
+    }
 
-    if(!$package.PublishedVersion) { return $true }
+    if(!$package.PublishedVersion) {
+        Write-BoxstarterMessage "$($package.Name) has not yet been published. It will be built and tested"
+        return $true 
+    }
 
     $pkgVersion=New-Object -TypeName Version -ArgumentList (Remove-PreRelease $package.Version)
     $pubVersion=New-Object -TypeName Version -ArgumentList (Remove-PreRelease $package.PublishedVersion)
 
     if($pkgVersion -gt $pubVersion) {
+        Write-BoxstarterMessage "The repository version '$($package.Version)' for $($package.Name) is greater than the published version '$($package.PublishedVersion)'. It will be built and tested"
         return $true 
     }
     else {
+        Write-BoxstarterMessage "The repository version '$($package.Version)' for $($package.Name) is not greater than the published version '$($package.PublishedVersion)'. It will not be built and tested"
         return $false 
     }
 }
