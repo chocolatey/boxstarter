0e5a5f715ebdd92cc523e309d6b3f80c608c0072
Matt Wrock <matt@mattwrock.com>
2014-02-24 00:50:33 -0800

    adding incremental testing

--- Boxstarter.CI/Test-BoxstarterPackage.ps1
+++ Boxstarter.CI/Test-BoxstarterPackage.ps1
@@ -1,18 +1,32 @@
-function Test-BoxstarterPackage($PackageName) {
+function Test-BoxstarterPackage {
+    [CmdletBinding()]
+    param(
+        [string[]]$PackageName
+    )
     $options = Get-BoxstarterDeployOptions
-    Invoke-BoxstarterBuild $PackageName
     if(!$options.DeploymentTargetCredentials){
         throw "No DeploymentTargetCredentials has been sent. Use Set-BoxstarterBuildOptions to set DeploymentTargetCredentials"
     }
-    $global:pester = @{arr_testTags=""}
-    $global:testName = ""
+
     if($options.DeploymentVMProvider -and $options.DeploymentVMProvider -gt 0){
         $vmArgs=@{}
         if($options.DeploymentCloudServiceName){$vmArgs.CloudServiceName=$options.DeploymentCloudServiceName}
         if($options.RestoreCheckpoint){$vmArgs.CheckpointName=$options.RestoreCheckpoint}
         $vmArgs.Provider=$options.DeploymentVMProvider
     }
+
+    #Hacks to get Pester to work with dynamicly created tests
+    $global:pester = @{arr_testTags=""}
+    $global:testName = ""
+
+    if(!$packageName){
+        $packageName = Get-BoxstarterPackages | ? {
+            Test-PackageVersionGreaterThanPublished $_
+        } | % {$_.Name}
+    }
+    
     $PackageName | % {
+        Invoke-BoxstarterBuild $_
         $p=$_
         Describe "Testing Package $p" {
             $options.DeploymentTargetNames | % {
@@ -33,5 +47,31 @@ function Test-BoxstarterPackage($PackageName) {
                     }
                 }
         }
+    }
+}
+
+function Test-PackageVersionGreaterThanPublished ($package) {
+    if(!$package.Feed) { return $false }
+
+    if(!$package.PublishedVersion) { return $true }
+
+    $pkgVersion=New-Object -TypeName Version -ArgumentList (Remove-PreRelease $package.Version)
+    $pubVersion=New-Object -TypeName Version -ArgumentList (Remove-PreRelease $package.PublishedVersion)
+
+    if($pkgVersion -gt $pubVersion) {
+        return $true 
+    }
+    else {
+        return $false 
+    }
+}
+
+function Remove-PreRelease ([string]$versionString) {
+    $idx=$versionString.IndexOf("-")
+    if($idx -gt -1) {
+        return $versionString.Substring(0,$idx)
+    }
+    else {
+        return $versionString 
     }
 }
