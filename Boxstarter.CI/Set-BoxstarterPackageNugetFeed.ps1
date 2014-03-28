411d3aba53648524c1c81e82d16066352d195019
Matt Wrock <matt@mattwrock.com>
2014-03-27 23:41:46 -0700

    adding documentation to Set-BoxstarterPackageNugetFeed

--- Boxstarter.CI/Set-BoxstarterPackageNugetFeed.ps1
+++ Boxstarter.CI/Set-BoxstarterPackageNugetFeed.ps1
@@ -1,4 +1,39 @@
 function Set-BoxstarterPackageNugetFeed {
+<#
+.SYNOPSIS
+Sets the Nuget feed associated with an individual package.
+
+.DESCRIPTION
+A Chocolatey package repository may publish different packages to 
+different feeds. Set-BoxstarterPackageNugetFeed sets the Nuget 
+feed associated with a specified package name. 
+Get-BoxstarterPackageNugetFeed can be used to retrieve the feed 
+associated with a package. One may also use Set-BoxstarterDeployOptions 
+and use the DefaultNugetFeed parameter to specify which feed to use for 
+a package if no feed is specified. If you do not want any feed to be 
+associated with a package, explicitly use Set-BoxstarterPackageNugetFeed 
+to set the feed of a package to $null.
+
+.PARAMETER PackageName
+The name of a Chocolatey package in the local repository for which a 
+feed should be associated.
+
+.PARAMETER NugetFeed
+The Nuget feed to associate with the Chocolatey package.
+
+.Example
+set-BoxstarterPackageNugetFeed -PackageName MyPackage `
+  -NugetFeed https://www.myget.org/F/mywackyfeed/api/v2
+
+Sets the package MyPackage to the MyGet.org mywackyfeed 
+
+.LINK
+http://boxstarter.codeplex.com
+Get-BoxstarterPackageNugetFeed
+Remove-BoxstarterPackageNugetFeed
+Set-BoxstarterDeployOptions
+Get-BoxstarterDeployOptions
+#>
     [CmdletBinding()]
     param (
         [string]$PackageName,
