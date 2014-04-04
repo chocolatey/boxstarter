c2061ddd2ae904be2155b1ccd0bf26823ab76615
Matt Wrock <matt@mattwrock.com>
2014-04-04 01:08:52 -0700

    make sure explorer restarts when restarted

--- Boxstarter.WinConfig/Restart-Explorer.ps1
+++ Boxstarter.WinConfig/Restart-Explorer.ps1
@@ -1,3 +1,7 @@
 function Restart-Explorer {
-    try{Stop-Process -processname explorer -Force -ErrorAction Stop | Out-Null} catch {}
+
+    try{
+        Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoRestartShell -Value 1 | out-Null
+        Stop-Process -processname explorer -Force -ErrorAction Stop | Out-Null
+    } catch {}
 }
