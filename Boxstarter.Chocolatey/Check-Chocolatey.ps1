c22a4e77724a73ac4596412c6432b481f592aac4
Matt Wrock <matt@mattwrock.com>
2014-04-05 00:29:53 -0700

    dotnet installer is broken for local instals. no longer

--- Boxstarter.Chocolatey/Check-Chocolatey.ps1
+++ Boxstarter.Chocolatey/Check-Chocolatey.ps1
@@ -31,9 +31,15 @@ function Enable-Net40 {
         Write-BoxstarterMessage "Downloading .net 4.5..."
         Get-HttpToFile "http://download.microsoft.com/download/b/a/4/ba4a7e71-2906-4b2d-a0e1-80cf16844f5f/dotnetfx45_full_x86_x64.exe" "$env:temp\net45.exe"
         Write-BoxstarterMessage "Installing .net 4.5..."
-        Invoke-FromTask @"
+        if(Get-IsRemote) {
+            Invoke-FromTask @"
 Start-Process "$env:temp\net45.exe" -verb runas -wait -argumentList "/quiet /norestart /log $env:temp\net45.log"
 "@
+        }
+        else {
+            $proc = Start-Process "$env:temp\net45.exe" -verb runas -argumentList "/quiet /norestart /log $env:temp\net45.log" -PassThru 
+            while(!$proc.HasExited){ sleep -Seconds 1 }
+        }
     }
 }
 
