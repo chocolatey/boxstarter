6b993269938ee9edc95abe9499ce5ca478353d29
Matt Wrock <matt@mattwrock.com>
2014-05-02 21:43:45 -0700

    If azure modile is among the available modules, import it from the pipeline since via the name arg seems to break

--- Boxstarter.Azure/BoxStarter.Azure.psm1
+++ Boxstarter.Azure/BoxStarter.Azure.psm1
@@ -2,7 +2,9 @@ $unNormalized=(Get-Item "$PSScriptRoot\..\Boxstarter.Chocolatey\Boxstarter.Choco
 Import-Module $unNormalized.FullName -global -DisableNameChecking -Force
 Resolve-Path $PSScriptRoot\*-*.ps1 | 
     % { . $_.ProviderPath }
-if((Get-Module Azure -ListAvailable) -eq $null) {
+
+$azureMod = Get-Module Azure -ListAvailable
+if(!$azureMod) {
     if(${env:ProgramFiles(x86)} -ne $null){ $programFiles86 = ${env:ProgramFiles(x86)} } else { $programFiles86 = $env:ProgramFiles }
     $modulePath="$programFiles86\Microsoft SDKs\Windows Azure\PowerShell\Azure\Azure.psd1"
     if(Test-Path $modulePath) {
@@ -10,6 +12,6 @@ if((Get-Module Azure -ListAvailable) -eq $null) {
     }
 }
 else {
-    Import-Module Azure -global
+    $azureMod | Import-Module -global
 }
 Export-ModuleMember Enable-BoxstarterVM, Get-AzureVMCheckpoint, Set-AzureVMCheckpoint, Restore-AzureVMCheckpoint, Remove-AzureVMCheckpoint, Test-VMStarted
