f0cbeb723ea1f39489819ef422f4a794460b6c65
Matt Wrock <matt@mattwrock.com>
2014-06-06 00:34:00 -0700

    if explorer does not restart after a second, manually start it

--- Boxstarter.WinConfig/Restart-Explorer.ps1
+++ Boxstarter.WinConfig/Restart-Explorer.ps1
@@ -1,8 +1,14 @@
 function Restart-Explorer {
 
     try{
-        Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoRestartShell -Value 1 | out-Null
+        #Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoRestartShell -Value 1 | out-Null
         $user = Get-CurrentUser
         Get-Process -Name explorer -IncludeUserName | ? { $_.UserName -eq "$($user.Domain)\$($user.Name)"} | Stop-Process -Force -ErrorAction Stop | Out-Null
+
+        Start-Sleep 1
+
+        if(!(Get-Process -Name explorer -ErrorAction SilentlyContinue)) {
+            start-Process -Name explorer
+        }
     } catch {}
 }
