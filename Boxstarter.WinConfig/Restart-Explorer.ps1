2cf37e6390d76439d3f44d6e008db0e2ab05d771
Matt Wrock <matt@mattwrock.com>
2014-07-04 19:10:05 -0700

    fix explorer restart when there is no explorer process

--- Boxstarter.WinConfig/Restart-Explorer.ps1
+++ Boxstarter.WinConfig/Restart-Explorer.ps1
@@ -3,7 +3,7 @@ function Restart-Explorer {
     try{
         #Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoRestartShell -Value 1 | out-Null
         $user = Get-CurrentUser
-        Get-Process -Name explorer -IncludeUserName | ? { $_.UserName -eq "$($user.Domain)\$($user.Name)"} | Stop-Process -Force -ErrorAction Stop | Out-Null
+        Get-Process -Name explorer -ErrorAction SilentlyContinue -IncludeUserName | ? { $_.UserName -eq "$($user.Domain)\$($user.Name)"} | Stop-Process -Force -ErrorAction Stop | Out-Null
 
         Start-Sleep 1
 
