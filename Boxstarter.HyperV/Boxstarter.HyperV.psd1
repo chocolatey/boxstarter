c381b796838ca8e512c92a8bd9280a3e0ab9318d
Matt Wrock <matt@mattwrock.com>
2014-12-26 03:21:21 -0800

    release 2.4.149

--- Boxstarter.HyperV/Boxstarter.HyperV.psd1
+++ Boxstarter.HyperV/Boxstarter.HyperV.psd1
@@ -4,7 +4,7 @@ Description = 'Provides Cmdlets that will install a Boxstarter package on a Hype
 ModuleToProcess = './boxstarter.HyperV.psm1'
 
 # Version number of this module.
-ModuleVersion = '2.4.146'
+ModuleVersion = '2.4.149'
 
 # ID used to uniquely identify this module
 GUID = 'bbdb3e8b-9daf-4c00-a553-4f3f88fb6e58'
@@ -43,7 +43,7 @@ AliasesToExport = '*'
 # FileList = @()
 
 # Private data to pass to the module specified in RootModule/ModuleToProcess
-PrivateData = '84c9fa548241464791f42b5355d660f1ea81ad7e'
+PrivateData = 'b95ad835fd8b8a21b5f0e753b7b95afa395ec74d'
 
 # Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
 # DefaultCommandPrefix = ''
--- Web/Properties/AssemblyInfo.cs
+++ Web/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Web")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("84c9fa548241464791f42b5355d660f1ea81ad7e")]
+[assembly: AssemblyTrademark("b95ad835fd8b8a21b5f0e753b7b95afa395ec74d")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.4.146")]
-[assembly: AssemblyFileVersion("2.4.146")]
+[assembly: AssemblyVersion("2.4.149")]
+[assembly: AssemblyFileVersion("2.4.149")]
