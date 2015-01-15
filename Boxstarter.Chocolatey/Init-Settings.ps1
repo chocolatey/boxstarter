56029cdb1fb4d317963373511eee855f54ff3292
Matt Wrock <matt@mattwrock.com>
2015-01-15 04:34:48 -0800

    if running as system put local repo in temp

--- Boxstarter.Chocolatey/Init-Settings.ps1
+++ Boxstarter.Chocolatey/Init-Settings.ps1
@@ -1,6 +1,10 @@
 $config = Get-BoxstarterConfig
 if(!$BoxStarter.LocalRepo){
-    $BoxStarter.LocalRepo=$config.LocalRepo
+   $BoxStarter.LocalRepo=$config.LocalRepo
+}
+if($BoxStarter.LocalRepo.StartsWith("$env:windir")) {
+   $BoxStarter.LocalRepo = Join-Path $(Get-BoxstarterTempDir) "BuildPackages"
+   if(!(Test-Path $BoxStarter.LocalRepo)) { mkdir $BoxStarter.LocalRepo | Out-Null }
 }
 $Boxstarter.NugetSources=$config.NugetSources
 $Boxstarter.RebootOk=$true
