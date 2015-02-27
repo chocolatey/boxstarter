7ba9d31b6f2bf874c6bb634ad73dd20ea8be1722
Matt Wrock <matt@mattwrock.com>
2015-02-27 14:35:29 -0800

    adding integration test with new choco install

--- IntegrationTests/EndToEnd.tests.ps1
+++ IntegrationTests/EndToEnd.tests.ps1
@@ -8,17 +8,37 @@ Describe "Invoke-ChocolateyBoxstarter via bootstrapper.bat (end to end)" {
 
     Context "When installing a local package" {
         remove-Item "$env:ChocolateyInstall\lib\test-package.*" -force -recurse
-        Add-Content "$env:ChocolateyInstall\ChocolateyInstall\ChocolateyInstall.log" -Value "______ test-package v1.0.0 ______" -force
 
         ."$here\..\boxstarter.bat" test-package -LocalRepo "$testRoot\Repo" -DisableReboots
 
         it "should save boxstarter package to chocolatey lib folder" {
             Test-Path "$env:ChocolateyInstall\lib\test-package.*" | Should Be $true
         }
-        it "should have cleared previous logs" {
-            $installLines = get-content "$env:ChocolateyInstall\ChocolateyInstall\Install.log" | ? { $_ -like "Successfully installed 'test-package*" } 
-            $installLines.Count | Should Be 1
-        }          
+    }
+}
+
+Describe "Install-BoxstarterPackage" {
+    $testRoot = (Get-PSDrive TestDrive).Root
+    $oldChoco = $env:ChocolateyInstall
+    $env:ChocolateyInstall = "$testRoot\choco"
+    $repo = "$testRoot\Repo"
+    mkdir $repo -ErrorAction SilentlyContinue | Out-Null
+    mkdir $env:ChocolateyInstall -ErrorAction SilentlyContinue | Out-Null
+    
+    Copy-Item $here\..\BuildPackages\test-package.*.nupkg $repo
+    Copy-Item $oldChoco\choco.exe $env:ChocolateyInstall
+
+    Context "When installing a local package" {
+        remove-Item "$env:ChocolateyInstall\lib\test-package.*" -force -recurse -ErrorAction SilentlyContinue
+
+        $result = Install-BoxstarterPackage -PackageName test-package -LocalRepo "$testRoot\Repo" -DisableReboots
+
+        it "should save boxstarter package to chocolatey lib folder" {
+            Test-Path "$env:ChocolateyInstall\lib\test-package" | Should Be $true
+        }
+        it "should not have errors" {
+            $result.errors.count | Should Be 0
+        }
     }
 }
 
