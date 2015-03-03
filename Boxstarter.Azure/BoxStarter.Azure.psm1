9a03eccc5adceb85e64f26d657aea2bdc52a7154
Matt Wrock <matt@mattwrock.com>
2015-03-03 09:04:01 -0800

    updating azure dependencies

--- Boxstarter.Azure/BoxStarter.Azure.psm1
+++ Boxstarter.Azure/BoxStarter.Azure.psm1
@@ -1,4 +1,4 @@
-$unNormalized=(Get-Item "$PSScriptRoot\..\Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1")
+catch$unNormalized=(Get-Item "$PSScriptRoot\..\Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1")
 Import-Module $unNormalized.FullName -global -DisableNameChecking -Force
 Resolve-Path $PSScriptRoot\*-*.ps1 | 
     % { . $_.ProviderPath }
@@ -9,7 +9,7 @@ try {Get-Module Storage -ListAvailable | Import-Module -global} catch { Log-Boxs
 $azureMod = Get-Module Azure -ListAvailable
 if(!$azureMod) {
     if(${env:ProgramFiles(x86)} -ne $null){ $programFiles86 = ${env:ProgramFiles(x86)} } else { $programFiles86 = $env:ProgramFiles }
-    $modulePath="$programFiles86\Microsoft SDKs\Windows Azure\PowerShell\Azure\Azure.psd1"
+    $modulePath="$programFiles86\Microsoft SDKs\Azure\PowerShell\ServiceManagement\Azure\Azure.psd1"
     if(Test-Path $modulePath) {
         Import-Module $modulePath -global
     }
