741aa256e434875af7e692eb21cd2a650d68cb54
Matt Wrock <matt@mattwrock.com>
2015-03-03 22:01:37 -0800

    fix typo in azure module

--- Boxstarter.Azure/BoxStarter.Azure.psm1
+++ Boxstarter.Azure/BoxStarter.Azure.psm1
@@ -1,4 +1,4 @@
-catch$unNormalized=(Get-Item "$PSScriptRoot\..\Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1")
+$unNormalized=(Get-Item "$PSScriptRoot\..\Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1")
 Import-Module $unNormalized.FullName -global -DisableNameChecking -Force
 Resolve-Path $PSScriptRoot\*-*.ps1 | 
     % { . $_.ProviderPath }
