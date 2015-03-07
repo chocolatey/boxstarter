3a729da5aa927c7e6fa577b85b2ef3cb3098917f
Igal Tabachnik <hmemcpy@gmail.com>
2015-03-07 23:13:15 +0200

    Changing 'if' to 'switch' to account for default values (i.e. it's not an if/else case, what about values that were never explicitly set?)

--- Boxstarter.Export/Export-WindowsExplorerOptions.ps1
+++ Boxstarter.Export/Export-WindowsExplorerOptions.ps1
@@ -15,13 +15,21 @@ http://boxstarter.org
 
 	$args = @()
 	if(Test-Path -Path $advancedKey) {
-        $args += if ((Get-ItemProperty $advancedKey).Hidden -eq 1) { "EnableShowHiddenFilesFoldersDrives" } else { "DisableShowHiddenFilesFoldersDrives" }
-        $args += if ((Get-ItemProperty $advancedKey).HideFileExt -eq 0) { "EnableShowFileExtensions" } else { "DisableShowFileExtensions" }
-        $args += if ((Get-ItemProperty $advancedKey).ShowSuperHidden -eq 1) { "EnableShowProtectedOSFiles" } else { "DisbleShowProtectedOSFiles" }
+        $args += switch ((Get-ItemProperty $advancedKey).Hidden) 
+                 { 1 {"EnableShowHiddenFilesFoldersDrives"} 
+                   0 {"DisableShowHiddenFilesFoldersDrives"} }
+        $args += switch ((Get-ItemProperty $advancedKey).HideFileExt) 
+                 { 0 {"EnableShowFileExtensions"} 
+                   1 {"DisableShowFileExtensions"} }
+        $args += switch ((Get-ItemProperty $advancedKey).ShowSuperHidden)
+                 { 1 {"EnableShowProtectedOSFiles"} 
+                   0 {"DisbleShowProtectedOSFiles"} }
 	}
 
     if(Test-Path -Path $cabinetStateKey) {
-        $args += if ((Get-ItemProperty $cabinetStateKey).FullPath -eq 1) { "EnableShowFullPathInTitleBar" } else { "DisableShowFullPathInTitleBar" }
+        $args += switch ((Get-ItemProperty $cabinetStateKey).FullPath) 
+                 { 1 {"EnableShowFullPathInTitleBar"} 
+                   0 {"DisableShowFullPathInTitleBar"} }
     }
     
     [PSCustomObject]@{"Command" = "Set-WindowsExplorerOptions"; "Arguments" = $args}
