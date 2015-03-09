49a9fc5c50f4e1bd0c684a3299019c3a6029b3ab
Igal Tabachnik <hmemcpy@gmail.com>
2015-03-09 09:04:56 +0200

    Adding basic export script. It prints to output, not really useful yet.

---
+++ Boxstarter.Export/Export-BoxstarterScript.ps1
@@ -0,0 +1,48 @@
+function Export-BoxstarterScript {
+<#
+.SYNOPSIS
+Exports the current box configuration as a Boxstarter script
+
+.DESCRIPTION
+
+.PARAMETER OutputFileName
+The name of the script to export
+
+.PARAMETER All
+Indicates that all settings should be exported
+
+.LINK
+http://boxstarter.org
+#>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,ParameterSetName='outputFileName')]
+        [string]$outputFileName,
+        [Parameter(Position=0,ParameterSetName='all')]
+        [switch]$all,
+        [switch]$quiet
+    )
+
+    $result = @()
+    Get-Command -Module Boxstarter.Export | % {
+        $obj = & $_
+        
+        $command = $obj.Command
+        $args = $obj.Arguments
+
+        if ($args -ne $null) {
+            $args = ($obj.Arguments | % { "-" + $_ })
+        }
+
+        if ($command -ne $null) {
+            $result += $($obj.Command + " " + $args) | Write-Host
+        }
+    }
+
+    # TODO: write the result to file
+    # $result
+
+}
+
+Export-BoxstarterScript -outputFileName "foo"
