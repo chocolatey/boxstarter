7647318e658d7963b605e4f4f3e55771276c6a4a
Matt Wrock <matt@mattwrock.com>
2015-04-02 00:07:05 -0700

    Dont throw exception when removing the scheduled task and it did not exist

--- BoxStarter.Common/Remove-BoxstarterTask.ps1
+++ BoxStarter.Common/Remove-BoxstarterTask.ps1
@@ -14,7 +14,14 @@ Invoke-BoxstarterTask
 
 #>    
     Write-BoxstarterMessage "Removing Boxstarter Scheduled Task..." -Verbose
-	$result = schtasks /DELETE /TN 'Boxstarter Task' /F 2>&1
+	$currentErrorCount = $Global:Error.Count
+    $result = schtasks /DELETE /TN 'Boxstarter Task' /F 2>&1
     Write-BoxstarterMessage "Removed Boxstarter Scheduled Task with this result: $result" -Verbose
 
+    if($Global:Error.Count -gt $currentErrorCount){
+        $limit = $Global:Error.Count - $currentErrorCount
+        for($i=0;$i -lt $limit;$i++) {
+            $Global:Error.RemoveAt(0)
+        }
+    }
 }
