4ce7a53119801e1bd7a8acb66a2ecbb123c2246f
Phil Campbell <philoushka@gmail.com>
2015-07-13 18:52:05 -0700

    remove test calls at bottom. more tabs -> spaces

--- Boxstarter.WinConfig/Set-WindowsExplorerOptions.ps1
+++ Boxstarter.WinConfig/Set-WindowsExplorerOptions.ps1
@@ -50,25 +50,25 @@ http://boxstarter.org
 
 #>   
 
-	[CmdletBinding()]
-	param(
-		[switch]$EnableShowHiddenFilesFoldersDrives,
-		[switch]$DisableShowHiddenFilesFoldersDrives,
-		[switch]$EnableShowProtectedOSFiles,
-		[switch]$DisableShowProtectedOSFiles,
-		[switch]$EnableShowFileExtensions,
-		[switch]$DisableShowFileExtensions,
-		[switch]$EnableShowFullPathInTitleBar,
-		[switch]$DisableShowFullPathInTitleBar,
-		[switch]$EnableOpenFileExplorerToQuickAccess,
-		[switch]$DisableOpenFileExplorerToQuickAccess,
-		[switch]$EnableShowRecentFilesInQuickAccess,
-		[switch]$DisableShowRecentFilesInQuickAccess,
-		[switch]$EnableShowFrequentFoldersInQuickAccess,
-		[switch]$DisableShowFrequentFoldersInQuickAccess
-	)
+    [CmdletBinding()]
+    param(
+        [switch]$EnableShowHiddenFilesFoldersDrives,
+        [switch]$DisableShowHiddenFilesFoldersDrives,
+        [switch]$EnableShowProtectedOSFiles,
+        [switch]$DisableShowProtectedOSFiles,
+        [switch]$EnableShowFileExtensions,
+        [switch]$DisableShowFileExtensions,
+        [switch]$EnableShowFullPathInTitleBar,
+        [switch]$DisableShowFullPathInTitleBar,
+        [switch]$EnableOpenFileExplorerToQuickAccess,
+        [switch]$DisableOpenFileExplorerToQuickAccess,
+        [switch]$EnableShowRecentFilesInQuickAccess,
+        [switch]$DisableShowRecentFilesInQuickAccess,
+        [switch]$EnableShowFrequentFoldersInQuickAccess,
+        [switch]$DisableShowFrequentFoldersInQuickAccess
+    )
 
-	$PSBoundParameters.Keys | % {
+    $PSBoundParameters.Keys | % {
         if($_-like "En*"){ $other="Dis" + $_.Substring(2)}
         if($_-like "Dis*"){ $other="En" + $_.Substring(3)}
         if($PSBoundParameters[$_] -and $PSBoundParameters[$other]) {
@@ -83,43 +83,35 @@ http://boxstarter.org
     Write-BoxstarterMessage "Setting Windows Explorer options..."
 
     if(Test-Path -Path $key) {
+        if($EnableShowRecentFilesInQuickAccess) {Set-ItemProperty $key ShowRecent 1}
+        if($DisableShowRecentFilesInQuickAccess) {Set-ItemProperty $key ShowRecent 0}
 
-		if($EnableShowRecentFilesInQuickAccess) {Set-ItemProperty $key ShowRecent 1}
-		if($DisableShowRecentFilesInQuickAccess) {Set-ItemProperty $key ShowRecent 0}
-		if($EnableShowFrequentFoldersInQuickAccess) {Set-ItemProperty $key ShowFrequent 1}
-		if($DisableShowFrequentFoldersInQuickAccess) {Set-ItemProperty $key ShowFrequent 0}
+        if($EnableShowFrequentFoldersInQuickAccess) {Set-ItemProperty $key ShowFrequent 1}
+        if($DisableShowFrequentFoldersInQuickAccess) {Set-ItemProperty $key ShowFrequent 0}
 
         Restart-Explorer
     }
 
-	if(Test-Path -Path $advancedKey) {
-		if($EnableShowHiddenFilesFoldersDrives) {Set-ItemProperty $advancedKey Hidden 1}
-		if($DisableShowHiddenFilesFoldersDrives) {Set-ItemProperty $advancedKey Hidden 0}
-		
-		if($EnableShowFileExtensions) {Set-ItemProperty $advancedKey HideFileExt 0}
-		if($DisableShowFileExtensions) {Set-ItemProperty $advancedKey HideFileExt 1}
-		
-		if($EnableShowProtectedOSFiles) {Set-ItemProperty $advancedKey ShowSuperHidden 1}
-		if($DisableShowProtectedOSFiles) {Set-ItemProperty $advancedKey ShowSuperHidden 0}
-		
+    if(Test-Path -Path $advancedKey) {
+        if($EnableShowHiddenFilesFoldersDrives) {Set-ItemProperty $advancedKey Hidden 1}
+        if($DisableShowHiddenFilesFoldersDrives) {Set-ItemProperty $advancedKey Hidden 0}
+
+        if($EnableShowFileExtensions) {Set-ItemProperty $advancedKey HideFileExt 0}
+        if($DisableShowFileExtensions) {Set-ItemProperty $advancedKey HideFileExt 1}
+
+        if($EnableShowProtectedOSFiles) {Set-ItemProperty $advancedKey ShowSuperHidden 1}
+        if($DisableShowProtectedOSFiles) {Set-ItemProperty $advancedKey ShowSuperHidden 0}
+
         if($EnableOpenFileExplorerToQuickAccess) {Set-ItemProperty $advancedKey LaunchTo 2}
-		if($DisableOpenFileExplorerToQuickAccess) {Set-ItemProperty $advancedKey LaunchTo 1}
+        if($DisableOpenFileExplorerToQuickAccess) {Set-ItemProperty $advancedKey LaunchTo 1}
 
-		Restart-Explorer
-	}
+        Restart-Explorer
+    }
 
-	if(Test-Path -Path $cabinetStateKey) {
-		if($EnableShowFullPathInTitleBar) {Set-ItemProperty $cabinetStateKey FullPath  1}
-		if($DisableShowFullPathInTitleBar) {Set-ItemProperty $cabinetStateKey FullPath  0}
-		
-		Restart-Explorer		
-	}
+    if(Test-Path -Path $cabinetStateKey) {
+        if($EnableShowFullPathInTitleBar) {Set-ItemProperty $cabinetStateKey FullPath  1}
+        if($DisableShowFullPathInTitleBar) {Set-ItemProperty $cabinetStateKey FullPath  0}
+
+        Restart-Explorer		
+    }
 }
-
-Set-WindowsExplorerOptions -EnableOpenFileExplorerToQuickAccess
-Set-WindowsExplorerOptions -EnableShowRecentFilesInQuickAccess
-Set-WindowsExplorerOptions -EnableShowFrequentFoldersInQuickAccess
-
-Set-WindowsExplorerOptions -DisableOpenFileExplorerToQuickAccess
-Set-WindowsExplorerOptions -DisableShowRecentFilesInQuickAccess
-Set-WindowsExplorerOptions -DisableShowFrequentFoldersInQuickAccess
