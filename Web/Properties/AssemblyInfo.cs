5a591e40914d33643cfaa16ea16246dbb1b871fd
Matt Wrock <matt@mattwrock.com>
2015-07-25 20:09:14 -0700

    releasing 2.5.19

--- Web/Properties/AssemblyInfo.cs
+++ Web/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Web")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("8d0e740ab9deb04fb2acb4634928975519ead6f1")]
+[assembly: AssemblyTrademark("8cce893c7d18e2150da3f242bceab9d819f67b32")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.5.10")]
-[assembly: AssemblyFileVersion("2.5.10")]
+[assembly: AssemblyVersion("2.5.19")]
+[assembly: AssemblyFileVersion("2.5.19")]
