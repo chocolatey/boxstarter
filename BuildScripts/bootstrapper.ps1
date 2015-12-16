a0026d17b0f8e70707b4cbf34dd83ec09f26db5e
Matt Wrock <matt@mattwrock.com>
2015-12-15 17:49:55 -0800

    fix bootstrapper type

--- BuildScripts/bootstrapper.ps1
+++ BuildScripts/bootstrapper.ps1
@@ -21,7 +21,7 @@ function Get-Boxstarter {
             # if there is no -v then its an older version with no -y
             $command = "cinst Boxstarter"
         }
-        $command += " -version 2.6.16
+        $command += " -version 2.6.16"
         Invoke-Expression $command
         $Message = "Boxstarter Module Installer completed"
     }
