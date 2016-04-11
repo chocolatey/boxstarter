0de31a8d57b9e43684aed1cc89458ece008ce7d3
Matt Wrock <matt@mattwrock.com>
2016-04-10 22:17:21 -0700

    pin azure libs used in build to 2.5

--- BuildScripts/build.ps1
+++ BuildScripts/build.ps1
@@ -14,7 +14,7 @@ if(!(Test-Path $env:ChocolateyInstall\lib\Psake*)) { cinst psake -y }
 if(!(Test-Path $env:ChocolateyInstall\lib\7zip.CommandLine*)) { cinst 7zip.CommandLine -y }
 if(!(Test-Path $env:ChocolateyInstall\lib\pester*)) { cinst pester -v 3.3.12 -y }
 if(!(Test-Path $env:ChocolateyInstall\lib\WindowsAzurePowershell*)) { cinst WindowsAzurePowershell -y }
-if(!(Test-Path $env:ChocolateyInstall\lib\WindowsAzureLibsForNet*)) { cinst WindowsAzureLibsForNet -y }
+if(!(Test-Path $env:ChocolateyInstall\lib\WindowsAzureLibsForNet*)) { cinst WindowsAzureLibsForNet -v 2.5 -y }
 if(!(Test-Path $env:ChocolateyInstall\bin\nuget.exe)) { cinst nuget.commandline -y }
 
 if($Help){ 
