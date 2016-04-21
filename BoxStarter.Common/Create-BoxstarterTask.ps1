d0997f562b3d1d3035e8c4337a3694e8c38be345
Matt Wrock <matt@mattwrock.com>
2016-04-21 00:02:40 -0700

    suppress error from checking if boxstarter task exits

--- BoxStarter.Common/Create-BoxstarterTask.ps1
+++ BoxStarter.Common/Create-BoxstarterTask.ps1
@@ -20,29 +20,31 @@ Remove-BoxstarterTask
 
 #>
     param([Management.Automation.PsCredential]$Credential)
-    if($Credential.GetNetworkCredential().Password.length -gt 0){
-        schtasks /CREATE /TN 'Temp Boxstarter Task' /SC WEEKLY /RL HIGHEST `
-            /RU "$($Credential.UserName)" /IT /RP $Credential.GetNetworkCredential().Password `
-        /TR "powershell -noprofile -ExecutionPolicy Bypass -File $env:temp\BoxstarterTask.ps1" /F | Out-Null
+    Remove-BoxstarterError {
+        if($Credential.GetNetworkCredential().Password.length -gt 0){
+            schtasks /CREATE /TN 'Temp Boxstarter Task' /SC WEEKLY /RL HIGHEST `
+                /RU "$($Credential.UserName)" /IT /RP $Credential.GetNetworkCredential().Password `
+            /TR "powershell -noprofile -ExecutionPolicy Bypass -File $env:temp\BoxstarterTask.ps1" /F | Out-Null
 
-        #Give task a normal priority
-        $taskFile = Join-Path $env:TEMP RemotingTask.txt
-        Remove-Item $taskFile -Force -ErrorAction SilentlyContinue
-        [xml]$xml = schtasks /QUERY /TN 'Temp Boxstarter Task' /XML
-        if($xml.Task.Settings.Priority -eq $null) {
-            $priority = $xml.CreateElement("Priority", "http://schemas.microsoft.com/windows/2004/02/mit/task")
-            $xml.Task.Settings.AppendChild($priority)
+            #Give task a normal priority
+            $taskFile = Join-Path $env:TEMP RemotingTask.txt
+            Remove-Item $taskFile -Force -ErrorAction SilentlyContinue
+            [xml]$xml = schtasks /QUERY /TN 'Temp Boxstarter Task' /XML
+            if($xml.Task.Settings.Priority -eq $null) {
+                $priority = $xml.CreateElement("Priority", "http://schemas.microsoft.com/windows/2004/02/mit/task")
+                $xml.Task.Settings.AppendChild($priority)
+            }
+            $xml.Task.Settings.Priority="4"
+            $xml.Save($taskFile)
+            schtasks /CREATE /TN 'Boxstarter Task' /RU "$($Credential.UserName)" /IT /RP $Credential.GetNetworkCredential().Password /XML "$taskFile" /F | Out-Null
+            schtasks /DELETE /TN 'Temp Boxstarter Task' /F | Out-Null
         }
-        $xml.Task.Settings.Priority="4"
-        $xml.Save($taskFile)
-        schtasks /CREATE /TN 'Boxstarter Task' /RU "$($Credential.UserName)" /IT /RP $Credential.GetNetworkCredential().Password /XML "$taskFile" /F | Out-Null
-        schtasks /DELETE /TN 'Temp Boxstarter Task' /F | Out-Null
-    }
-    elseif(!((schtasks /QUERY /TN 'Boxstarter Task' /FO LIST 2>&1) -contains 'Logon Mode:    Interactive/Background')) { #For testing
-        schtasks /CREATE /TN 'Boxstarter Task' /SC WEEKLY /RL HIGHEST `
-                /RU "$($Credential.UserName)" /IT `
-        /TR "powershell -noprofile -ExecutionPolicy Bypass -File $env:temp\BoxstarterTask.ps1" /F |
-                Out-Null
+        elseif(!((schtasks /QUERY /TN 'Boxstarter Task' /FO LIST 2>&1) -contains 'Logon Mode:    Interactive/Background')) { #For testing
+            schtasks /CREATE /TN 'Boxstarter Task' /SC WEEKLY /RL HIGHEST `
+                    /RU "$($Credential.UserName)" /IT `
+            /TR "powershell -noprofile -ExecutionPolicy Bypass -File $env:temp\BoxstarterTask.ps1" /F |
+                    Out-Null
+        }
     }
     if($LastExitCode -gt 0){
         throw "Unable to create scheduled task as $($Credential.UserName)"
