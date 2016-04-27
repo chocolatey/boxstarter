d9b52fba428aca12fd49f95bab45796fa6920ec5
Steven Maglio <smaglio81@gmail.com>
2016-04-27 08:48:15 -0700

    Replaced -RunAsTask with Invoke-BoxstarterScriptFromTask

--- Boxstarter.Chocolatey/Boxstarter.Chocolatey.psm1
+++ Boxstarter.Chocolatey/Boxstarter.Chocolatey.psm1
@@ -18,6 +18,7 @@ Export-ModuleMember Get-BoxstarterConfig,`
                     Export-BoxstarterVars,`
                     Install-ChocolateyInstallPackageOverride,`
                     Invoke-BoxstarterBuild,`
+                    Invoke-BoxstarterFromTask,`
                     Invoke-Chocolatey,`
                     Invoke-ChocolateyBoxstarter,`
                     Install-BoxstarterPackage,`
