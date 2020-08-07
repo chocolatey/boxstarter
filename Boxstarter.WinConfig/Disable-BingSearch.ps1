f975c7d2d51b8fca896f599791669cb902822ab1
Paul Broadwith <paul@pauby.com>
2020-08-07 17:01:36 +0100

    (GH-434) Fix disabling bing search for Win release 2004+

--- Boxstarter.WinConfig/Disable-BingSearch.ps1
+++ Boxstarter.WinConfig/Disable-BingSearch.ps1
@@ -1,19 +1,42 @@
-function Disable-BingSearch {
 <#
 .SYNOPSIS
-Disables the Bing Internet Search when searching from the search field in the Taskbar or Start Menu.
+Disables the Bing Internet Search when using the search field in the Taskbar or Start Menu.
+
+.DESCRIPTION
+Disables the Bing Internet Search when using the search field in the Taskbar or Start Menu.
+
+This is usable on all Windows versions pre and post Windows 2004 release (OS version 10.0.19041).
+
+.NOTES
+Boxstarter (https://boxstarter.org) (c) 2018 Chocolatey Software, Inc, 2012 - 2018 Matt Wrock.
 
 .LINK
 https://boxstarter.org
 https://www.privateinternetaccess.com/forum/discussion/18301/how-to-uninstall-core-apps-in-windows-10-and-miscellaneous
+https://www.lifehacker.com.au/2020/06/how-to-disable-bing-search-in-windows-10s-start-menu-2/
 
 #>
-    $path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search"
+function Disable-BingSearch {
+    [CmdletBinding()]
+    Param()
 
-    if(!(Test-Path $path)) {
-        New-Item $path
+    $path = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search'
+    $windows2004AndLaterPath = 'HKCU:\Software\Policies\Microsoft\Windows\Explorer'
+    $windows2004Version = '10.0.19041'
+
+    $osVersion = (Get-CimInstance -ClassName Win32_OperatingSystem).Version
+    if ([version]$osVersion -ge [version]$windows2004Version) {
+        if (-not (Test-Path -Path $windows2004AndLaterPath)) {
+            $null = New-Item -Path $windows2004AndLaterPath
+        }
+
+        $null = New-ItemProperty -Path $windows2004AndLaterPath -Name 'DisableSearchBoxSuggestions' -Value 1 -PropertyType 'DWORD'
     }
+    else {
+        if( -not (Test-Path -Path $path)) {
+            $null = New-Item -Path $path
+        }
 
-    New-ItemProperty -LiteralPath $path -Name "BingSearchEnabled" -Value 0 -PropertyType "DWord" -ErrorAction SilentlyContinue
-    Set-ItemProperty -LiteralPath $path -Name "BingSearchEnabled" -Value 0
+        $null = New-ItemProperty -Path $path -Name "BingSearchEnabled" -Value 0 -PropertyType "DWORD"
+    }
 }
