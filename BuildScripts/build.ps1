36a43831f739f7a3774c0e997f0860a081751c87
Gary Ewan Park <gep13@gep13.co.uk>
2020-08-17 13:58:43 -0700

    (build) Remove deprecated package

--- BuildScripts/build.ps1
+++ BuildScripts/build.ps1
@@ -11,7 +11,7 @@ if(-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall")){
 }
 
 if(!(Test-Path $env:ChocolateyInstall\lib\Psake*)) { cinst psake -y --no-progress }
-if(!(Test-Path $env:ChocolateyInstall\lib\7zip.CommandLine*)) { cinst 7zip.CommandLine -y --no-progress }
+if(!(Test-Path $env:ChocolateyInstall\lib\7zip.portable*)) { cinst 7zip.portable -y --no-progress }
 if(!(Test-Path $env:ChocolateyInstall\lib\pester*)) { cinst pester -y --no-progress }
 if(!(Test-Path $env:ChocolateyInstall\lib\AzurePowershell*)) { cinst AzurePowershell -y --no-progress }
 if(!(Test-Path $env:ChocolateyInstall\lib\WindowsAzureLibsForNet*)) { cinst WindowsAzureLibsForNet --version 2.5 -y --no-progress }
