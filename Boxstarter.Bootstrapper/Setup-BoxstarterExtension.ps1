02d0827313fb81fcd1433d769e584758af95d27d
Manfred Wallner <schusterfredl@mwallner.net>
2021-11-20 00:02:50 +0100

    fix recursive extension loading

--- Boxstarter.Bootstrapper/Setup-BoxstarterExtension.ps1
+++ Boxstarter.Bootstrapper/Setup-BoxstarterExtension.ps1
@@ -4,8 +4,13 @@ function Setup-BoxstarterExtension {
     New-Item -ItemType Directory -Path $extensionBaseDir -Force | Out-Null
 
     @"
-Write-Host "Lets.GetBoxstarter() :-)"        
-Import-Module '$($boxstarter.BaseDir)\Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1' -DisableNameChecking -ArgumentList `$true
+`$testfile = '$(Join-Path $env:temp "Boxstarter.ext.$PID")'
+
+if (-Not (Test-Path `$testfile)) {
+  New-Item -Path `$testfile | Out-Null
+  Write-Host "Lets.GetBoxstarter() :-)"        
+  Import-Module '$($boxstarter.BaseDir)\Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1' -Global -DisableNameChecking -ArgumentList `$true
+}
 "@ | Out-File "$extensionBaseDir/boxstarter-choco.psm1" -Encoding utf8
 
 }
