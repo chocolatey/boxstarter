38fa6ca12fa393a18a98d542fee8565e781793a8
Joshua King <windosbk+github@gmail.com>
2021-12-14 08:27:58 +1300

    (GH-488) Add TeamCity build config

---
+++ .teamcity/settings.kts
@@ -0,0 +1,77 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.powerShell
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2021.2"
+
+project {
+
+    vcsRoot(HttpsGithubComChocolateyBoxstarterGitRefsHeadsMaster)
+
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    artifactRules = "buildArtifacts => buildArtifacts"
+
+    vcs {
+        root(HttpsGithubComChocolateyBoxstarterGitRefsHeadsMaster)
+    }
+
+    steps {
+        powerShell {
+            name = "Prerequisites"
+            scriptMode = script {
+                content = """
+                    if ((Get-WindowsFeature -Name NET-Framework-Core).InstallState -ne 'Installed') {
+                        Install-WindowsFeature -Name NET-Framework-Core
+                    }
+                """.trimIndent()
+            }
+        }
+        powerShell {
+            name = "Build"
+            scriptMode = file {
+                path = "BuildScripts/build.ps1"
+            }
+            param("jetbrains_powershell_scriptArguments", "quick-deploy")
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
+
+object HttpsGithubComChocolateyBoxstarterGitRefsHeadsMaster : GitVcsRoot({
+    name = "https://github.com/chocolatey/boxstarter.git#refs/heads/master"
+    url = "https://github.com/chocolatey/boxstarter.git"
+    branch = "refs/heads/master"
+    branchSpec = "refs/heads/*"
+})
