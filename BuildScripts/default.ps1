1af41b5d08e78fe376713b1b588eec0e561f31c0
Gary Ewan Park <gep13@gep13.co.uk>
2022-04-26 10:55:58 +0100

    (maint) Remove build steps that aren't required
    There are some historical build steps that are no longer required, due
    to the new process that we are working towards. Steps like FTP to the
    web server, deploy using web-deploy, publishing the GitHub release,
    etc, will all be replaced with new parts of the process, so they
    aren't required anymore.

--- BuildScripts/default.ps1
+++ BuildScripts/default.ps1
@@ -18,7 +18,6 @@ properties {
     $script:informationalVersion = $version
     $script:changeset = "abcdef"
     $nugetExe = "$env:ChocolateyInstall\bin\nuget.exe"
-    $ftpHost="waws-prod-bay-001.ftp.azurewebsites.windows.net"
     $msbuildExe="${env:programFiles(x86)}\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe"
     $reportUnitExe = "$env:ChocolateyInstall\bin\ReportUnit.exe"
     $gitVersionExe = "$env:ChocolateyInstall\lib\GitVersion.Portable\tools\gitversion.exe"
@@ -26,11 +25,11 @@ properties {
 
 Task default -depends Build
 Task Build -depends Run-GitVersion, Build-Clickonce, Build-Web, Install-ChocoPkg, Test, Package
-Task Deploy -depends Build, Deploy-DownloadZip, Deploy-Bootstrapper, Publish-Clickonce, Update-Homepage -description 'Versions, packages and pushes to MyGet'
+Task Deploy -depends Build, Deploy-DownloadZip, Deploy-Bootstrapper, Publish-Clickonce -description 'Versions, packages and pushes'
 Task Package -depends Clean-Artifacts, Version-Module, Install-ChocoPkg, Create-ModuleZipForRemoting, Pack-NuGet, Package-DownloadZip -description 'Versions the psd1 and packs the module and example package'
-Task Push-Public -depends Push-Chocolatey, Push-GitHub, Publish-Web
+Task Push-Public -depends Push-Chocolatey
 Task All-Tests -depends Test, Integration-Test
-Task Quick-Deploy -depends Run-GitVersion, Build-Clickonce, Build-web, Package, Deploy-DownloadZip, Deploy-Bootstrapper, Publish-Clickonce, Update-Homepage
+Task Quick-Deploy -depends Run-GitVersion, Build-Clickonce, Build-web, Package, Deploy-DownloadZip, Deploy-Bootstrapper, Publish-Clickonce
 
 task Run-GitVersion {
     Write-Host "Testing to see if running on TeamCity..."
@@ -123,10 +122,6 @@ task Publish-ClickOnce -depends Install-MSBuild {
     Copy-Item "$basedir\Boxstarter.Clickonce\bin\Debug\App.Publish\*" "$basedir\web\Launch" -Recurse -Force
 }
 
-task Publish-Web -depends Install-MSBuild, Install-WebDeploy {
-    exec { .$msbuildExe "$baseDir\Web\Web.csproj" /p:DeployOnBuild=true /p:PublishProfile="boxstarter - Web Deploy" /p:VisualStudioVersion=12.0 /p:Password=$env:BOXSTARTER_PUBLISH_PASSWORD }
-}
-
 Task Test -depends Install-ChocoPkg, Pack-NuGet, Create-ModuleZipForRemoting {
     Push-Location "$baseDir"
     $pesterDir = "$env:ChocolateyInstall\lib\Pester"
@@ -199,7 +194,7 @@ Task Pack-NuGet -depends Sign-PowerShellFiles -description 'Packs the modules an
     }
 
     PackDirectory "$baseDir\BuildPackages"
-    PackDirectory "$baseDir\BuildScripts\nuget" -AddReleaseNotes -Version $packageVersion
+    PackDirectory "$baseDir\BuildScripts\nuget" -Version $packageVersion
     Move-Item "$baseDir\BuildScripts\nuget\*.nupkg" "$basedir\buildArtifacts"
 }
 
@@ -228,66 +223,8 @@ Task Deploy-Bootstrapper {
 Task Push-Chocolatey -description 'Pushes the module to Chocolatey community sfeed' {
     exec {
         Get-ChildItem "$baseDir\buildArtifacts\*.nupkg" |
-            ForEach-Object { cpush $_  }
+            ForEach-Object { choco push $_  }
     }
-}
-
-Task Push-GitHub {
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
-    $headers = @{
-        Authorization = 'Basic ' + [Convert]::ToBase64String(
-            [Text.Encoding]::ASCII.GetBytes("$($env:BOXSTARTER_GITHUB_USERNAME):$($env:BOXSTARTER_GITHUB_TOKEN)"));
-    }
-
-    $releaseNotes = Get-ReleaseNotes
-    $postParams = ConvertTo-Json @{
-        tag_name="v$packageVersion"
-        target_commitish=$changeset
-        name="v$packageVersion"
-        body=$releaseNotes.DocumentElement.'#text'
-    } -Compress
-
-    $latest = Invoke-RestMethod -Uri "https://api.github.com/repos/chocolatey/boxstarter/releases/latest" -Method GET -Headers $headers
-    if($latest.tag_name -ne "v$packageVersion"){
-        Write-Host "Creating release"
-        $response = Invoke-RestMethod -Uri "https://api.github.com/repos/chocolatey/boxstarter/releases" -Method POST -Body $postParams -Headers $headers
-        $uploadUrl = $response.upload_url.replace("{?name,label}","?name=boxstarter.$packageVersion.zip")
-    }
-    else {
-        $uploadUrl = $latest.upload_url.replace("{?name,label}","?name=boxstarter.$packageVersion.zip")
-    }
-
-    Write-Host "Uploading $basedir\BuildArtifacts\Boxstarter.$packageVersion.zip to $uploadUrl"
-    try {
-        Invoke-RestMethod -Uri $uploadUrl -Method POST -ContentType "application/zip" -InFile "$basedir\BuildArtifacts\Boxstarter.$packageVersion.zip" -Headers $headers
-    }
-    catch{
-        write-host $_ | Format-List * -force
-    }
-}
-
-task Update-Homepage {
-     $versionPattern="[0-9]+(\.([0-9]+|\*)){1,3}"
-     $filename = "$baseDir\web\App_Code\Helper.cshtml"
-     (Get-Content $filename) | ForEach-Object {$_ -replace $versionPattern, ($version) } | Set-Content $filename
-}
-
-task Get-ClickOnceStats {
-    $creds = Get-Credential
-    mkdir "$basedir\sitelogs" -ErrorAction silentlycontinue
-    Push-Location "$basedir\sitelogs"
-    $ftpScript = @"
-user $($creds.UserName) $($creds.GetNetworkCredential().Password)
-cd LogFiles/http/RawLogs
-mget *
-bye
-"@
-    $ftpScript | ftp -i -n $ftpHost
-    if(!(Test-Path $env:ChocolateyInstall\lib\logparser*)) { choco install logparser --version=2.2.0.1 -y --no-progress }
-    $logParser = "${env:programFiles(x86)}\Log Parser 2.2\LogParser.exe"
-    .$logparser -i:w3c "SELECT Date, EXTRACT_VALUE(cs-uri-query,'package') as package, COUNT(*) as count FROM * where cs-uri-stem = '/launch/Boxstarter.WebLaunch.Application' Group by Date, package Order by Date, package" -rtp:-1
-    Pop-Location
-    Remove-Item "$basedir\sitelogs" -Recurse -Force
 }
 
 task Install-MSBuild {
@@ -299,10 +236,6 @@ task Install-MSBuild {
 
 task Install-Win8SDK {
     if(!(Test-Path "$env:ProgramFiles\Windows Kits\8.1\bin\x64\signtool.exe")) { choco install windows-sdk-8.1 --version=8.100.26654.0 -y --no-progress }
-}
-
-task Install-WebDeploy {
-    if(!(Test-Path "$env:ProgramW6432\IIS\Microsoft Web Deploy V3")) { choco install webdeploy --version=3.6.20170627 -y --no-progress }
 }
 
 Task Restore-NuGetPackages {
@@ -370,25 +303,13 @@ task Sign-PowerShellFiles -depends Copy-PowerShellFiles {
     }
 }
 
-function PackDirectory($path, [switch]$AddReleaseNotes, $Version = $version){
+function PackDirectory($path, $Version = $version){
     exec {
-        $releaseNotes = Get-ReleaseNotes
         Get-ChildItem $path -Recurse -include *.nuspec |
             ForEach-Object {
-                 if($AddReleaseNotes) {
-                   [xml]$nuspec = Get-Content $_
-                   $oldReleaseNotes = $nuspec.package.metadata.ChildNodes| Where-Object { $_.Name -eq 'releaseNotes' }
-                   $newReleaseNotes = $nuspec.ImportNode($releaseNotes.DocumentElement, $true)
-                   $nuspec.package.metadata.ReplaceChild($newReleaseNotes, $oldReleaseNotes) | Out-Null
-                   $nuspec.Save($_)
-                 }
                  .$nugetExe pack $_ -OutputDirectory $path -NoPackageAnalysis -version $Version
               }
     }
-}
-
-function Get-ReleaseNotes {
-    [xml](Get-Content "$baseDir\BuildScripts\releaseNotes.xml")
 }
 
 # Borrowed from Luis Rocha's Blog (http://www.luisrocha.net/2009/11/setting-assembly-version-with-windows.html)
