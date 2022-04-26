0c8bbb3f24ca5f3d3e9bcd005f4467e2913cab5c
Gary Ewan Park <gep13@gep13.co.uk>
2022-04-26 07:33:53 +0100

    (build) Update AssemblyInfo.cs files
    To include more correct Copyright notice, as well as to add the
    AssemblyInformationalVersion attribute.  This will be updated as part
    of the build process, using the version number that comes from
    GitVersion.

--- Web/Properties/AssemblyInfo.cs
+++ Web/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Web")]
@@ -10,12 +10,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Web")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("(c) 2018 - Present Chocolatey Software, Inc, 2012 - 2018 Matt Wrock")]
 [assembly: AssemblyTrademark("8459d49a8d5a049d8936519ccf045706c7b3eb23")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -25,11 +25,12 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("2.13.0")]
 [assembly: AssemblyFileVersion("2.13.0")]
+[assembly: AssemblyInformationalVersion("2.13.0")]
