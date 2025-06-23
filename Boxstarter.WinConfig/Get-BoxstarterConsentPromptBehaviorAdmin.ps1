0ecb37dab035a7ff7bbe89140fc2e396311b0aae
Manfred Wallner <schusterfredl@mwallner.net>
2025-06-23 17:18:21 +0200

    gh-581 adjusted ConsentPromptBehaviorAdmin to match reference system

--- Boxstarter.WinConfig/Get-BoxstarterConsentPromptBehaviorAdmin.ps1
+++ Boxstarter.WinConfig/Get-BoxstarterConsentPromptBehaviorAdmin.ps1
@@ -2,13 +2,6 @@ function Get-BoxstarterConsentPromptBehaviorAdmin {
     <#
 https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-gpsb/341747f5-6b5d-4d30-85fc-fa1cc04038d4
 
-	
-0x00000000 -> "NeverNotify"
-0x00000001 -> "NotifyOnAppInstallWithoutDimming"
-0x00000002 -> "NotifyOnAppInstall"
-0x00000003 -> "PromptForConsent"
-0x00000004 -> "PromptForCredentials"
-0x00000005 -> "AlwaysNotify"
     #>
 
     $state = Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -ErrorAction SilentlyContinue
@@ -18,11 +11,9 @@ https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-gpsb/341747f5-6
     }
     switch ($state.ConsentPromptBehaviorAdmin) {
         0 { return 'NeverNotify' }
-        1 { return 'NotifyOnAppInstallWithoutDimming' }
-        2 { return 'NotifyOnAppInstall' }
-        3 { return 'PromptForConsent' }
-        4 { return 'PromptForCredentials' }
-        5 { return 'AlwaysNotify' }
+        # 5 { return 'NotifyOnAppInstallWithoutDimming' }
+        5 { return 'NotifyOnAppInstall' }
+        2 { return 'AlwaysNotify' }
         default {
             Write-BoxstarterMessage "Unknown ConsentPromptBehaviorAdmin value: $($state.ConsentPromptBehaviorAdmin). Defaulting to 'AlwaysNotify'."
             return 'AlwaysNotify'
--- Boxstarter.WinConfig/Set-BoxstarterConsentPromptBehaviorAdmin.ps1
+++ Boxstarter.WinConfig/Set-BoxstarterConsentPromptBehaviorAdmin.ps1
@@ -5,14 +5,6 @@ Sets the User Account Control (UAC) consent prompt behavior to specified value.
 
 https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-gpsb/341747f5-6b5d-4d30-85fc-fa1cc04038d4
 
-	
-0x00000000 -> "NeverNotify"
-0x00000001 -> "NotifyOnAppInstallWithoutDimming"
-0x00000002 -> "NotifyOnAppInstall"
-0x00000003 -> "PromptForConsent"
-0x00000004 -> "PromptForCredentials" 
-0x00000005 -> "AlwaysNotify"
-
 .LINK
 https://boxstarter.org
 
@@ -26,11 +18,9 @@ https://boxstarter.org
     )
     $value = switch ($ConsentPromptBehavior) {
         'NeverNotify' { 0 }
-        'NotifyOnAppInstallWithoutDimming' { 1 }
-        'NotifyOnAppInstall' { 2 }
-        'PromptForConsent' { 3 }
-        'PromptForCredentials' { 4 }
-        'AlwaysNotify' { 5 }
+        # 'NotifyOnAppInstallWithoutDimming' { 5 }
+        'NotifyOnAppInstall' { 5 }
+        'AlwaysNotify' { 2 }
     }
     Write-BoxstarterMessage "Setting ConsentPromptBehaviorAdmin to $ConsentPromptBehavior"
     Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System `
